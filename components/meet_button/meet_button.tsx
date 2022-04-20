342f5e99c55d272a2c0a692023376e99f6d8eaae
anton.buksa <anton.buksa@infomaniak.com>
2022-04-20 18:49:05 +0200

    add basic call window

--- components/meet_button/meet_button.tsx
+++ components/meet_button/meet_button.tsx
@@ -1,10 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React from 'react';
+import React, {useRef} from 'react';
 import {injectIntl, IntlShape} from 'react-intl';
 
 import {Channel, ChannelMembership} from 'mattermost-redux/types/channels';
 import CameraIcon from 'components/widgets/icons/camera_icon';
+import JitsiMeetExternalAPI from '../../packages/kmeet/external_api.js';
 
 type Props = {
     currentChannel: Channel;
@@ -13,13 +14,32 @@ type Props = {
     locale: string;
 }
 
+const configOverwrite = {
+    startWithAudioMuted: false,
+    startWithVideoMuted: true,
+    subject: 'toto',
+};
+
 function MeetButton(props: Props) {
     const {formatMessage} = props.intl;
+    const ref = useRef<HTMLButtonElement>(null);
+
+    const onClick = () => {
+        const options = {
+            windowName: 'kmeet',
+            windowOptions: 'location=0,status=0,width=900,height=500',
+            callback: () => console.log('callback kmeet'),
+        };
+
+        window.open('/static/call.html', options.windowName, options.windowOptions);
+    };
 
     return (
         <button
             type='button'
             className='style--none post-action'
+            onClick={onClick}
+            ref={ref}
         >
             <div
                 className='icon icon--attachment'
