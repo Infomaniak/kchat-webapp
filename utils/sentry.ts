cbf3b9d4bd68f1269dc3e80a734406e7d7abd12f
anton.buksa <anton.buksa@infomaniak.com>
2022-05-30 15:05:49 +0200

    tweak config

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -27,18 +27,12 @@ export default function init({SENTRY_DSN}: Args) {
         normalizeDepth: 5,
         integrations: [new BrowserTracing()],
         tracesSampleRate: 1.0,
-
-        // Disable client reports. Client reports are used by sentry to retry events that failed to send on visibility change.
-        // Unfortunately Sentry does not use the custom transport for those, and thus fails to add the headers the API requires.
-        sendClientReports: false,
         ignoreErrors: [
 
             // Ignore random plugins/extensions
             'top.GLOBALS',
             'canvas.contentDocument',
             'MyApp_RemoveAllHighlights',
-            'atomicFindClose',
-            'conduitPage',
 
             // https://bugzilla.mozilla.org/show_bug.cgi?id=1678243
             'XDR encoding failure',
@@ -47,13 +41,13 @@ export default function init({SENTRY_DSN}: Args) {
             'Failed to fetch',
             'NetworkError when attempting to fetch resource.',
             'No network connection',
-            'webkitExitFullScreen', // Bug in Firefox for iOS.
+            'webkitExitFullScreen',
             'InactiveSession',
-            'UnhandledRejection', // Happens too often in extensions and we have lints for that, so should be safe to ignore.
+            'UnhandledRejection',
             /chrome-extension/,
             /moz-extension/,
             'ChunkLoadError', // WebPack loading source code.
-            /ResizeObserver loop/, // Chromium bug https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded
+            /ResizeObserver loop/,
         ],
     });
 }
