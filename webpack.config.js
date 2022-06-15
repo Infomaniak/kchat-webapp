5216972c49519186c07b987ba3f6a37359b98e8f
Glauda Florent <florent.glauda@infomaniak.com>
2022-06-15 11:09:45 +0200

    WIP: Try fix SW scope error

--- webpack.config.js
+++ webpack.config.js
@@ -318,10 +318,10 @@ var config = {
         // Generate manifest.json, honouring any configured publicPath. This also handles injecting
         // <link rel="apple-touch-icon" ... /> and <meta name="apple-*" ... /> tags into root.html.
         new WebpackPwaManifest({
-            name: 'Mattermost',
-            short_name: 'Mattermost',
+            name: 'kChat',
+            short_name: 'kChat',
             start_url: '..',
-            description: 'Mattermost is an open source, self-hosted Slack-alternative',
+            description: 'Infomaniak kChat',
             background_color: '#ffffff',
             inject: true,
             ios: true,
