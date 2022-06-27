c2c51282aa161b2fbab33679cd36eb9ec5e4bb27
anton.buksa <anton.buksa@infomaniak.com>
2022-06-27 19:47:08 +0200

    disable option

--- components/user_settings/display/user_settings_display.tsx
+++ components/user_settings/display/user_settings_display.tsx
@@ -1008,7 +1008,7 @@ export default class UserSettingsDisplay extends React.PureComponent<Props, Stat
                     {linkPreviewSection}
                     {collapseSection}
                     {messageDisplaySection}
-                    {clickToReply}
+                    {/* {clickToReply} */}
                     {channelDisplayModeSection}
                     {oneClickReactionsOnPostsSection}
                     {/* {languagesSection} */}
