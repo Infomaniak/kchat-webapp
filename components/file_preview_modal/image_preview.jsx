852f122ba44f34243cfa0460a0f4265625896c1b
Dorian Santa Cruz <dorian.scd@infomaniak.com>
2022-07-27 16:20:14 +0200

    Functionnal canvas zoom

--- components/file_preview_modal/image_preview.jsx
+++ components/file_preview_modal/image_preview.jsx
@@ -1,20 +1,38 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+/* eslint-disable react-hooks/exhaustive-deps */
+
+/*
+
+to do:
+
+- Clamp displacement to corners
+- Zoom to where mouse is
+- Add toolbox (zoom in, out, current zoom level...)
+- Improve zoomToFit, make dat boxed in
+- Hand cursor (open when hover, closed when dragging)
+- Prevent default drag (dont hilight)
+- Spacebar toggles dragging?
+- Investigate mobile zoom in?
+- Add rotation?
+
+*/
+
 import PropTypes from 'prop-types';
 import React, {useRef, useMemo, useEffect, useState} from 'react';
 
 import {getFilePreviewUrl, getFileDownloadUrl} from 'mattermost-redux/utils/file_utils';
 
+import './image_preview.scss';
+
+const HORIZONTAL_PADDING = 100;
+const VERTICAL_PADDING = 168;
+
 const SCROLL_SENSITIVITY = 0.0005;
 const MAX_ZOOM = 5;
-const MIN_ZOOM = 0.1;
-
-//import './image_preview.scss';
-
-function clamp(num, min, max) {
-    return Math.min(Math.max(num, min), max);
-}
+var MAX_CANVAS_ZOOM = 1;
+var MIN_ZOOM = 1;
 
 export default function ImagePreview({fileInfo}) {
     const isExternalFile = !fileInfo.id;
@@ -22,10 +40,12 @@ export default function ImagePreview({fileInfo}) {
     const [offset, setOffset] = useState({x: 0, y: 0});
     const [zoom, setZoom] = useState(1);
     const [draggind, setDragging] = useState(false);
+
     const touch = useRef({x: 0, y: 0});
     const canvasRef = useRef(null);
     const containerRef = useRef(null);
     const observer = useRef(null);
+
     const background = useMemo(() => new Image(), []);
 
     let fileUrl;
@@ -37,6 +57,33 @@ export default function ImagePreview({fileInfo}) {
         fileUrl = getFileDownloadUrl(fileInfo.id);
         previewUrl = fileInfo.has_preview_image ? getFilePreviewUrl(fileInfo.id) : fileUrl;
     }
+
+    const clamp = (num, min, max) => {
+        return Math.min(Math.max(num, min), max);
+    };
+
+    const getWindowDimensions = () => {
+        const maxWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - HORIZONTAL_PADDING;
+        const maxHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - VERTICAL_PADDING;
+        return {maxWidth, maxHeight};
+    };
+
+    // cleanup, not pretty
+    const fitCanvas = (width, height) => {
+        const {maxWidth, maxHeight} = getWindowDimensions();
+
+        const scaleX = maxWidth / width;
+        const scaleY = maxHeight / height;
+
+        return Math.min(scaleX, scaleY);
+    };
+
+    const initCanvas = (width, height) => {
+        const containerScale = fitCanvas(width, height);
+        MIN_ZOOM = containerScale <= 1 ? containerScale : 1;
+        MAX_CANVAS_ZOOM = containerScale;
+        return MIN_ZOOM;
+    };
 
     const handleWheel = (event) => {
         event.persist();
@@ -68,70 +115,92 @@ export default function ImagePreview({fileInfo}) {
 
     useEffect(() => {
         observer.current = new ResizeObserver((entries) => {
-            entries.forEach(({target}) => {
-                const {width, height} = background;
+            // Request animation frame to avoid spamming console with loop warnings
+            window.requestAnimationFrame(() => {
+                if (!Array.isArray(entries) || !entries.length) {
+                    return;
+                }
 
-                // If width of the container is smaller than image, scale image down
-                if (target.clientWidth < width) {
-                    // Calculate scale
-                    const scale = target.clientWidth / width;
+                entries.forEach(() => {
+                    const {width, height} = background;
+                    const containerScale = initCanvas(width, height);
 
-                    // Redraw image
-                    canvasRef.current.width = width * scale;
-                    canvasRef.current.height = height * scale;
+                    canvasRef.current.width = width * containerScale;
+                    canvasRef.current.height = height * containerScale;
                     canvasRef.current.
                         getContext('2d').
-                        drawImage(background, 0, 0, width * scale, height * scale);
-                }
+                        drawImage(background, 0, 0, width * containerScale, height * containerScale);
+                });
             });
         });
         observer.current.observe(containerRef.current);
+        const currentContainer = containerRef.current;
 
-        return () => observer.current.unobserve(containerRef.current);
+        return () => observer.current.unobserve(currentContainer);
     }, [background]);
 
     useEffect(() => {
         background.src = previewUrl;
 
         if (canvasRef.current) {
+            // Better AA, could be interesting: https://stackoverflow.com/questions/17861447/html5-canvas-drawimage-how-to-apply-antialiasing
+
             background.onload = () => {
                 // Get the image dimensions
                 const {width, height} = background;
-                canvasRef.current.width = width;
-                canvasRef.current.height = height;
+                const containerScale = initCanvas(width, height);
 
-                // Set image as background
-                canvasRef.current.getContext('2d').drawImage(background, 0, 0);
+                canvasRef.current.width = width * containerScale;
+                canvasRef.current.height = height * containerScale;
+
+                // Initialize with the zoom at minimum.
+                setZoom(MIN_ZOOM);
+
+                // Set image as background and scale accordingly
+                canvasRef.current.getContext('2d').drawImage(background, 0, 0, width * containerScale, height * containerScale);
             };
         }
     }, [background, previewUrl]);
 
     useEffect(() => {
         if (canvasRef.current) {
-            const {width, height} = canvasRef.current;
             const context = canvasRef.current.getContext('2d');
 
-            // Set canvas dimensions
-            canvasRef.current.width = width;
-            canvasRef.current.height = height;
+            // We have not reached the limit, we can make canvas bigger
+            if (zoom < MAX_CANVAS_ZOOM) {
+                const {width, height} = background;
 
-            // Clear canvas and scale it
-            context.translate(-offset.x, -offset.y);
-            context.scale(zoom, zoom);
-            context.clearRect(0, 0, width, height);
+                canvasRef.current.width = width * zoom;
+                canvasRef.current.height = height * zoom;
 
-            // Make sure we're zooming to the center
-            const x = ((context.canvas.width / zoom) - background.width) / 2;
-            const y = ((context.canvas.height / zoom) - background.height) / 2;
+                // Draw image
+                context.drawImage(background, 0, 0, width * zoom, height * zoom);
+            } else {
+                const {width, height} = canvasRef.current;
 
-            // Draw image
-            context.drawImage(background, x, y);
+                // Set canvas dimensions
+                canvasRef.current.width = width;
+                canvasRef.current.height = height;
+
+                // Clear canvas and scale it
+                context.translate(-offset.x, -offset.y);
+                context.scale(zoom, zoom);
+                context.clearRect(0, 0, width, height);
+
+                // Make sure we're zooming to the center
+                const x = ((context.canvas.width / zoom) - background.width) / 2;
+                const y = ((context.canvas.height / zoom) - background.height) / 2;
+
+                // Draw image
+                context.drawImage(background, x, y);
+            }
         }
     }, [zoom, offset, background]);
 
     return (
         <div
             ref={containerRef}
+            className='image_preview__image'
         >
             <canvas
                 onMouseDown={handleMouseDown}
