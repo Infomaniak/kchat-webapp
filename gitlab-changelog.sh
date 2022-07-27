ddedae92a932e56c777b00d5b2d391138a87ef0e
anton.buksa <anton.buksa@infomaniak.com>
2022-07-27 16:25:12 +0200

    clean changelog script

--- gitlab-changelog.sh
+++ gitlab-changelog.sh
@@ -1,17 +1,21 @@
+# Considering that your app version is stored in VERSION.md file
+#
+# Example: ./gitlab-changelog.sh
+# Example: ./gitlab-changelog.sh <YOUR-GITLAB-PROJECT-ACCESS-TOKEN>
+
 #!/bin/bash
 
 latest=$(git describe --tags --abbrev=0)
-
-# echo "Latest: ${latest}"
+branch=master
 
 # You can retrieve env variables from script arguments (here 1st argument)
 GITLAB_PROJECT_ACCESS_TOKEN=$1
 GITLAB_PROJECT_TAG=$2
 
-response=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}&branch=master&from=${latest}&to=release" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
+response=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}&branch=${branch}&from=${latest}&to=release" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
 
 echo $response
 # if [ $response == 200 ]
-# then echo "Updated changelog"
+# then echo "Updated changelog: ${link}"
 # else echo "An error occurred when requesting GitLab API"
 # fi;
