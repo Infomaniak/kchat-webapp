f888397bb46fc3712a2f0ae791bb422ac0f28bd0
anton.buksa <anton.buksa@infomaniak.com>
2022-07-27 16:57:06 +0200

    Revert "Update for prod"
    This reverts commit 89a0d5b335864f2d56fce043e00d44cc160e0cfb.

--- utils/constants-ik.jsx
+++ utils/constants-ik.jsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 export const IKConstants = {
-    LOGIN_URL: 'https://login.infomaniak.com/',
+    LOGIN_URL: 'https://login.preprod.dev.infomaniak.ch/',
     CLIENT_ID: 'A7376A6D-9A79-4B06-A837-7D92DB93965B',
 };
