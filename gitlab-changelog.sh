186602efcf9353ae829b3bae130b53aa34be03bd
anton.buksa <anton.buksa@infomaniak.com>
2022-08-03 17:57:08 +0200

    combine release script & comment tests

--- gitlab-changelog.sh
+++ gitlab-changelog.sh
@@ -4,7 +4,17 @@ latest=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1  --max-cou
 
 GITLAB_PROJECT_ACCESS_TOKEN=$1
 GITLAB_PROJECT_TAG=$2
+MILESTONE=$3
 
-response=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}&from=${latest}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
+postchangelog=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}&from=${latest}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
+changelog=$(curl --request GET --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog" | jq -r '.notes')
+release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=${GITLAB_PROJECT_TAG}&tag_name=${GITLAB_PROJECT_TAG}&description=${response}&milestones=${MILESTONE}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/releases")
+data=$(jq -n --arg text "$changelog" '{"text": $text}')
+notify=$(curl -f -X POST -H "Content-Type: application/json" --data "$DATA" --silent --output /dev/null ${SLACK_WEBHOOK_URL})
 
-echo $response
+
+echo $postchangelog
+echo $changelog
+echo $release
+echo $notify
+
