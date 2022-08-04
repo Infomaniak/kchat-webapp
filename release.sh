046ce3af5b342e8ea53512b3d5d2a0e4886baf68
anton.buksa <anton.buksa@infomaniak.com>
2022-08-04 10:48:21 +0200

    improve changelog
    improve changelog
    
    Changelog: chore

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ GITLAB_PROJECT_ACCESS_TOKEN=$1
 GITLAB_PROJECT_TAG=$2
 MILESTONE=$3
 
-postchangelog=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}&from=${latest}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
+postchangelog=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog")
 changelog=$(curl --request GET --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/repository/changelog" | jq -r '.notes')
 release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=${GITLAB_PROJECT_TAG}&tag_name=${GITLAB_PROJECT_TAG}&description=${changelog}&milestones=${MILESTONE}" "https://gitlab.infomaniak.ch/api/v4/projects/3225/releases")
 data=$(jq -n --arg text "$changelog" '{"text": $text}')
