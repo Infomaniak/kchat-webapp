f700a90451b69a799869e188dfbc4d5a12bbd986
anton.buksa <anton.buksa@infomaniak.com>
2022-08-26 17:20:08 +0200

    clean unused tests

--- components/user_settings/advanced/user_settings_advanced.test.tsx
+++ components/user_settings/advanced/user_settings_advanced.test.tsx
@@ -69,35 +69,6 @@ describe('components/user_settings/display/UserSettingsDisplay', () => {
         expect(updateSection).toHaveBeenCalledWith('linkpreview');
     });
 
-    test('should have called updateUserActive', () => {
-        const updateUserActive = jest.fn(() => Promise.resolve({}));
-        const props = {...requiredProps, actions: {...requiredProps.actions, updateUserActive}};
-        const wrapper = shallow<AdvancedSettingsDisplay>(<AdvancedSettingsDisplay {...props}/>);
-
-        wrapper.instance().handleDeactivateAccountSubmit();
-        expect(updateUserActive).toHaveBeenCalled();
-        expect(updateUserActive).toHaveBeenCalledWith(requiredProps.currentUser.id, false);
-    });
-
-    test('handleDeactivateAccountSubmit() should have called revokeAllSessions', () => {
-        const wrapper = shallow<AdvancedSettingsDisplay>(<AdvancedSettingsDisplay {...requiredProps}/>);
-
-        wrapper.instance().handleDeactivateAccountSubmit();
-        expect(requiredProps.actions.revokeAllSessionsForUser).toHaveBeenCalled();
-        expect(requiredProps.actions.revokeAllSessionsForUser).toHaveBeenCalledWith(requiredProps.currentUser.id);
-    });
-
-    test('handleDeactivateAccountSubmit() should have updated state.serverError', async () => {
-        const error = {message: 'error'};
-        const revokeAllSessionsForUser = () => Promise.resolve({error});
-        const props = {...requiredProps, actions: {...requiredProps.actions, revokeAllSessionsForUser}};
-        const wrapper = shallow<AdvancedSettingsDisplay>(<AdvancedSettingsDisplay {...props}/>);
-
-        await wrapper.instance().handleDeactivateAccountSubmit();
-
-        expect(wrapper.state().serverError).toEqual(error.message);
-    });
-
     test('function getCtrlSendText should return correct value for Mac', () => {
         (isMac as jest.Mock).mockReturnValue(true);
         const props = {...requiredProps};
