3c4d6b3a1fc259a7a208d2c87588c09634b2f3da
anton.buksa <anton.buksa@infomaniak.com>
2022-09-22 15:38:03 +0200

    add left product module in web
    Changelog: added

--- components/global_header/left_controls/left_controls.tsx
+++ components/global_header/left_controls/left_controls.tsx
@@ -27,6 +27,33 @@ const LeftControlsContainer = styled.div`
     .multi-teams & {
         width: 255px
     }
+
+    .no-webcomponents & {
+        width: 255px
+    }
+`;
+
+const HeaderIcon = styled.span`
+    height: 31px;
+    display: flex;
+    align-items: center;
+    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    justify-content: center;
+    padding-right: 6px;
+    > i {
+        cursor: pointer;
+        background-color: #000;
+        color: rgba(var(--sidebar-text-rgb), 0.72);
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        position: relative;
+        border-radius: 50%;
+        display: -webkit-box;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `;
 
 const LeftControls = (): JSX.Element => (
@@ -34,6 +61,24 @@ const LeftControls = (): JSX.Element => (
         {/* <ProductMenu/> */}
         <AppNameDisplay/>
         {isDesktopApp() && <HistoryButtons/>}
+        {!isDesktopApp() && (
+            <div
+                style={{position: 'relative', marginLeft: 'auto', marginRight: 9}}
+            >
+                {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                {/* @ts-ignore */}
+                <module-products-component position='right'>
+                    <HeaderIcon
+                        className='header-icon'
+                        slot='trigger'
+                    >
+                        <i className='icon icon-chevron-down'/>
+                    </HeaderIcon>
+                    {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                    {/* @ts-ignore */}
+                </module-products-component>
+            </div>
+        )}
     </LeftControlsContainer>
 );
 
