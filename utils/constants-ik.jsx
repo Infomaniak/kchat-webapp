4f2b7c872af6f044a457012599fde02d29308682
anton.buksa <anton.buksa@infomaniak.com>
2022-09-26 15:09:38 +0200

    improve env for ikconstants

--- utils/constants-ik.jsx
+++ utils/constants-ik.jsx
@@ -1,9 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+declare const LOGIN_ENDPOINT: string;
+declare const MANAGER_ENDPOINT: string;
+
 export const IKConstants = {
-    LOGIN_URL: process.env.LOGIN_ENDPOINT,
-    LOGOUT_URL: `${process.env.LOGIN_ENDPOINT}logout`,
+    LOGIN_URL: LOGIN_ENDPOINT,
+    LOGOUT_URL: `${LOGIN_ENDPOINT}logout`,
     CLIENT_ID: 'A7376A6D-9A79-4B06-A837-7D92DB93965B',
-    MANAGER_URL: process.env.MANAGER_ENDPOINT,
+    MANAGER_URL: MANAGER_ENDPOINT,
 };
