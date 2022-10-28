ed8c72a8dc1ef1280fb237166d8f64e1835126f0
anton.buksa <anton.buksa@infomaniak.com>
2022-10-28 14:46:54 +0200

    improve root redirect

--- components/root/root.tsx
+++ components/root/root.tsx
@@ -176,6 +176,36 @@ export default class Root extends React.PureComponent<Props, State> {
         // Redux
         setUrl(getSiteURL());
 
+        if (isDesktopApp()) {
+            const token = localStorage.getItem('IKToken');
+            const refreshToken = localStorage.getItem('IKRefreshToken');
+            const tokenExpire = localStorage.getItem('IKTokenExpire');
+
+            // Enable authHeader and set bearer token
+            if (token && tokenExpire && !checkIKTokenIsExpired()) {
+                Client4.setAuthHeader = true;
+                Client4.setToken(token);
+                Client4.setCSRF(token);
+                LocalStorageStore.setWasLoggedIn(true);
+                window.postMessage(
+                    {
+                        type: 'token-refreshed',
+                        message: {
+                            token,
+                        },
+                    },
+                    window.origin,
+                );
+            }
+
+            // If need to refresh the token
+            if ((tokenExpire && checkIKTokenIsExpired()) || !token || !refreshToken) {
+                this.props.history.push('/login' + this.props.location.search);
+            }
+        } else {
+            Client4.setAuthHeader = false; // Disable auth header to enable CSRF check
+        }
+
         setSystemEmojis(new Set(EmojiIndicesByAlias.keys()));
 
         // Force logout of all tabs if one tab is logged out
@@ -392,19 +422,6 @@ export default class Root extends React.PureComponent<Props, State> {
                     window.origin,
                 );
             }
-
-            // if (!token && !refreshToken) {
-            //     console.log('[TOKEN] No token, redirect to login 3');
-            //     window.postMessage(
-            //         {
-            //             type: 'browser-history-push',
-            //             message: {
-            //                 path: '/login',
-            //             },
-            //         },
-            //         window.location.origin,
-            //     );
-            // }
         }
         this.mounted = true;
 
