400b1329c638a099ba3be637e12d17ee5ea55fef
anton.buksa <anton.buksa@infomaniak.com>
2022-11-11 17:16:54 +0100

    dont load sentry on devs

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -11,7 +11,7 @@ interface Args {
 // Webpack global var
 declare const COMMIT_HASH: string;
 
-const isLocalhost = (host: string) => host.startsWith('localhost') || host.startsWith('local.');
+const isLocalhost = (host: string) => host.startsWith('localhost') || host.startsWith('local.') || host.startsWith('kchat.devd');
 
 export default function init({SENTRY_DSN}: Args) {
     const {host} = window.location;
