262817d2c97ca23552c00e6953aaa3f923a60c03
anton.buksa <anton.buksa@infomaniak.com>
2022-11-18 13:18:32 +0100

    fix header

--- packages/client/src/websocket.ts
+++ packages/client/src/websocket.ts
@@ -278,28 +278,28 @@ export default class WebSocketClient {
 
     subscribeToTeamChannel(teamId: string) {
         // @ts-ignore
-        this.conn?.config.auth?.headers = `Bearer ${localStorage.getItem('IKToken')}`;
+        this.conn?.config.auth?.headers?.Authorization = `Bearer ${localStorage.getItem('IKToken')}`;
         // @ts-ignore
         this.teamChannel = this.conn.subscribe(`private-team.${teamId}`);
     }
 
     subscribeToUserChannel(userId: number) {
         // @ts-ignore
-        this.conn?.config.auth?.headers = `Bearer ${localStorage.getItem('IKToken')}`;
+        this.conn?.config.auth?.headers?.Authorization = `Bearer ${localStorage.getItem('IKToken')}`;
         // @ts-ignore
         this.userChannel = this.conn.subscribe(`presence-user.${userId}`);
     }
 
     subscribeToPresenceChannel(channelID: string) {
         // @ts-ignore
-        this.conn?.config.auth?.headers = `Bearer ${localStorage.getItem('IKToken')}`;
+        this.conn?.config.auth?.headers?.Authorization = `Bearer ${localStorage.getItem('IKToken')}`;
         // @ts-ignore
         this.presenceChannel = this.conn.subscribe(`presence-channel.${channelID}`);
     }
 
     bindPresenceChannel(channelID: string) {
         // @ts-ignore
-        this.conn?.config.auth?.headers = `Bearer ${localStorage.getItem('IKToken')}`;
+        this.conn?.config.auth?.headers?.Authorization = `Bearer ${localStorage.getItem('IKToken')}`;
         // @ts-ignore
         this.presenceChannel = this.conn?.subscribe(`presence-channel.${channelID}`);
         if (this.presenceChannel) {
