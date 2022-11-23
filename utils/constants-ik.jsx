d95a6dc26f29bf8c97d78a893405b27017d8387e
Glauda Florent <florent.glauda@infomaniak.com>
2022-11-23 08:31:22 +0100

    rollback login preprod

--- utils/constants-ik.jsx
+++ utils/constants-ik.jsx
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 export const IKConstants = {
-    LOGIN_URL: 'https://login.devd254.dev.infomaniak.ch/',
+    LOGIN_URL: process.env.LOGIN_ENDPOINT,
     LOGOUT_URL: `${process.env.LOGIN_ENDPOINT}logout`,
     CLIENT_ID: 'A7376A6D-9A79-4B06-A837-7D92DB93965B',
     MANAGER_URL: process.env.MANAGER_ENDPOINT,
