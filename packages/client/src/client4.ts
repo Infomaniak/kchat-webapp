343f8538ed75a7795da8b21112936607187be5e7
Léonard <leonard.jouve@infomaniak.com>
2022-11-25 10:41:31 +0100

    call keepAlive on Webview
    Changelog: added

--- packages/client/src/client4.ts
+++ packages/client/src/client4.ts
@@ -4358,6 +4358,11 @@ export default class Client4 {
             console.log('[client > getChallengeAndRedirectToLogin] Error redirect');
         });
     }
+
+    // Only for Webview
+    keepAlive() {
+        this.doFetch(`${this.getBaseRoute()}/keepalive`, {method: 'get'});
+    }
 }
 
 export function parseAndMergeNestedHeaders(originalHeaders: any) {
