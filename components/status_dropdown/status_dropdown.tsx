9cb307e511f712f8ff95e8700649d40521c92748
anton.buksa <anton.buksa@infomaniak.com>
2022-12-07 16:44:17 +0100

    Revert "setup test with logout 2"
    This reverts commit 42a576a7f981fb07ee13a6c87f53175f620c1579.

--- components/status_dropdown/status_dropdown.tsx
+++ components/status_dropdown/status_dropdown.tsx
@@ -194,7 +194,7 @@ export default class StatusDropdown extends React.PureComponent<Props, State> {
     };
 
     handleEmitUserLoggedOutEvent = (): void => {
-        GlobalActions.emitUserLoggedOutEvent('/login');
+        GlobalActions.emitUserLoggedOutEvent('ikLogout');
     }
 
     onToggle = (open: boolean): void => {
