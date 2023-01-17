2eb01dcaf302282258c54eef10b341f0b11c4a1e
Léonard <leonard.jouve@infomaniak.com>
2023-01-17 16:36:37 +0100

    disable webpack overlay

--- webpack.config.js
+++ webpack.config.js
@@ -622,6 +622,9 @@ if (targetIsDevServer) {
             historyApiFallback: {
                 index: '/static/root.html',
             },
+            client: {
+                overlay: false,
+            },
         },
         performance: false,
         optimization: {
