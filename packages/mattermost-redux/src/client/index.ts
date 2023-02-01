a165fc76d8e1ee290385b7e11515a9ae955f259a
anton.buksa <anton.buksa@infomaniak.com>
2023-02-01 11:11:54 +0100

    dont need const declare

--- packages/mattermost-redux/src/client/index.ts
+++ packages/mattermost-redux/src/client/index.ts
@@ -7,8 +7,6 @@ import {
     DEFAULT_LIMIT_BEFORE,
 } from '@mattermost/client';
 
-declare const GIT_RELEASE: string;
-
 const Client4 = new ClientClass4();
 Client4.setWebappVersion(GIT_RELEASE);
 
