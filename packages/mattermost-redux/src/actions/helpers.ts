107e66b647bce8525fe563abacab37ffd1714ddc
Léonard <leonard.jouve@infomaniak.com>
2023-02-01 15:17:34 +0100

    add error id check on 401

--- packages/mattermost-redux/src/actions/helpers.ts
+++ packages/mattermost-redux/src/actions/helpers.ts
@@ -35,9 +35,9 @@ function redirectToErrorPageIfNecessary(err: ServerError) {
         getHistory().replace('/error?type=maintenance');
         break;
     case statusCodes.HTTP_BLOCKED:
-        getHistory().replace('/error?type=blocked');
-        break;
-    default:
+        if (err.server_error_id === 'product_locked' || err.id === 'product_locked') {
+            getHistory().replace('/error?type=blocked');
+        }
         break;
     }
 }
