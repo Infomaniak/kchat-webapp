ff004736f6fae990888b912d43dab924d9b8375c
Léonard <leonard.jouve@infomaniak.com>
2023-02-21 10:37:06 +0100

    rework channel limit modal message

--- components/limits/channel_limit_reached_modal/channel_limit_reached_modal.tsx
+++ components/limits/channel_limit_reached_modal/channel_limit_reached_modal.tsx
@@ -21,7 +21,12 @@ import useGetLimits from 'components/common/hooks/useGetLimits';
 
 import '../limit_modal.scss';
 
-const ChannelLimitReachedModal = () => {
+type Props = {
+    isPublicLimited?: boolean;
+    isPrivateLimited?: boolean;
+};
+
+const ChannelLimitReachedModal = ({isPublicLimited, isPrivateLimited}: Props) => {
     const {formatMessage} = useIntl();
     const dispatch = useDispatch();
 
@@ -67,21 +72,26 @@ const ChannelLimitReachedModal = () => {
 
     const content = (
         <div className='limit-modal-content'>
-            {formatMessage({
-                id: 'channelLimit.subtitle',
-                defaultMessage: 'You have reached the limit of public channels ({publicChannelUsage, number}/{publicChannelLimit, number}) and private channels ({privateChannelUsage, number}/{privateChannelLimit, number}) on your kChat. To create additional ones, you need to subscribe to a premium package.',
-            }, {
-                publicChannelUsage,
-                publicChannelLimit,
-                privateChannelUsage,
-                privateChannelLimit,
-            })}
+            <div>
+                {formatMessage({
+                    id: 'channelLimit.subtitle',
+                    defaultMessage: 'You have reached the limit of <bold>{isPublicLimited, select, true {public channels ({publicChannelUsage, number}/{publicChannelLimit, number}) {isPrivateLimited, select, true {and} other {}}} other {}} {isPrivateLimited, select, true {private channels ({privateChannelUsage, number}/{privateChannelLimit, number})} other {}}</bold> on your kChat. To create additional ones, you need to subscribe to a premium package.',
+                }, {
+                    isPublicLimited,
+                    isPrivateLimited,
+                    publicChannelUsage,
+                    publicChannelLimit,
+                    privateChannelUsage,
+                    privateChannelLimit,
+                    bold: (chunks) => (<b>{chunks}</b>),
+                })}
+            </div>
         </div>
     );
 
     const confirmButtonText = isAdmin ? formatMessage({id: 'limitModal.upgrade', defaultMessage: 'Modify my offer'}) : formatMessage({id: 'general_button.close', defaultMessage: 'Close'});
 
-    if (!usageLoaded || !limitsLoaded) {
+    if (!usageLoaded || !limitsLoaded || (!isPublicLimited && !isPrivateLimited)) {
         handleClose();
         return null;
     }
