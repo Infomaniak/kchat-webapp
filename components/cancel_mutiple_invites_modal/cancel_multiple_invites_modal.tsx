e4b24a5452704b3ea3fcd908d164e45ef8bb4e98
Léonard <leonard.jouve@infomaniak.com>
2023-03-03 09:59:58 +0100

    cancel multiple invites modal

--- components/cancel_mutiple_invites_modal/cancel_multiple_invites_modal.tsx
+++ components/cancel_mutiple_invites_modal/cancel_multiple_invites_modal.tsx
@@ -4,14 +4,24 @@
 import React, {useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
+import {Modal} from 'react-bootstrap';
+import styled from 'styled-components';
 
 import {cancelPendingGuestInvite} from 'mattermost-redux/actions/channels';
+import {makeGetChannelsForIds} from 'mattermost-redux/selectors/entities/channels';
 import {closeModal} from 'actions/views/modals';
 import {isModalOpen} from 'selectors/views/modals';
 import {GlobalState} from 'types/store';
-import {ModalIdentifiers} from 'utils/constants';
+import Constants, {ModalIdentifiers} from 'utils/constants';
 
-import {GenericModal} from '@mattermost/components';
+const StyledChannel = styled.div`
+    padding: 10px 12px;
+    font-weight: bold;
+    border-radius: 8px;
+    :hover {
+        background-color: rgba(var(--center-channel-color-rgb),0.08);
+    }
+`;
 
 type Props = {
     currentChannelId: string;
@@ -22,6 +32,8 @@ type Props = {
 const CancelMultipleInvitesModal = ({currentChannelId, channelIds, pendingGuestKey}: Props) => {
     const dispatch = useDispatch();
     const show = useSelector((state: GlobalState) => isModalOpen(state, ModalIdentifiers.CANCEL_MULTIPLE_INVITES));
+    const getChannelsForIds = makeGetChannelsForIds();
+    const channels = useSelector((state: GlobalState) => getChannelsForIds(state, channelIds));
 
     const handleClose = useCallback(() => {
         dispatch(closeModal(ModalIdentifiers.CANCEL_MULTIPLE_INVITES));
@@ -29,32 +41,73 @@ const CancelMultipleInvitesModal = ({currentChannelId, channelIds, pendingGuestK
 
     const handleCancelPendingGuestInvite = useCallback(() => {
         dispatch(cancelPendingGuestInvite(currentChannelId, pendingGuestKey));
-    }, [dispatch, currentChannelId, pendingGuestKey]);
+        handleClose();
+    }, [dispatch, currentChannelId, pendingGuestKey, handleClose]);
 
-    const header = (
-        <FormattedMessage
-            id='cancel_multiple_invites_modal'
-            defaultMessage='Cancel multiple invitations'
-        />
-    );
+    const channelList = channels.map((channel) => (
+        <StyledChannel key={`cancel-multiple-invites-channel-${channel.id}`}>
+            {channel.type === Constants.PRIVATE_CHANNEL ? (
+                <i className='icon icon-lock-outline'/>
+            ) : (
+                <i className='icon icon-globe'/>
+            )}
+            {channel.display_name}
+        </StyledChannel>
+    ));
 
     return (
-        <GenericModal
+        <Modal
             id='CancelMultipleInvitesModal'
             show={show}
+            onHide={handleClose}
             onExited={handleClose}
-            handleCancel={handleClose}
-            handleConfirm={handleCancelPendingGuestInvite}
-            modalHeaderText={header}
         >
-            <FormattedMessage
-                id='cancel_multiple_invites_modal'
-                defaultMessage='By cancelling this invitation, you will also cancel invitations in the following channels:\n{channelList}'
-                values={{
-                    channelList: channelIds.join(),
-                }}
-            />
-        </GenericModal>
+            <Modal.Header closeButton={true}>
+                <Modal.Title
+                    componentClass='h1'
+                    id='cancelMultipleInvitesModal'
+                >
+                    <FormattedMessage
+                        id='cancel_multiple_invites_modal.header'
+                        defaultMessage='Cancel multiple invitations'
+                    />
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className='alert alert-danger'>
+                    <FormattedMessage
+                        id='cancel_multiple_invites_modal.content'
+                        defaultMessage='By cancelling this invitation, you will also cancel invitations in the following channels :'
+                    />
+                </div>
+                <div className='pb-1'>
+                    {channelList}
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <button
+                    type='button'
+                    className='btn btn-link secondary'
+                    onClick={handleClose}
+                >
+                    <FormattedMessage
+                        id='generic_modal.cancel'
+                        defaultMessage='Cancel'
+                    />
+                </button>
+                <button
+                    type='button'
+                    className='btn btn-primary'
+                    onClick={handleCancelPendingGuestInvite}
+                    autoFocus={true}
+                >
+                    <FormattedMessage
+                        id='generic_modal.confirm'
+                        defaultMessage='Confirm'
+                    />
+                </button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
