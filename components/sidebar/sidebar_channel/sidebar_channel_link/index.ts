3883c036221c658aa2cef4fb6b01a962fbb40cad
Léonard <leonard.jouve@infomaniak.com>
2023-03-03 11:32:28 +0100

    Guest onboarding
    Changelog: fixed
    move channels tutorial step to channels sidebar category

--- components/sidebar/sidebar_channel/sidebar_channel_link/index.ts
+++ components/sidebar/sidebar_channel/sidebar_channel_link/index.ts
@@ -10,13 +10,10 @@ import {GenericAction} from 'mattermost-redux/types/actions';
 import {isChannelMuted} from 'mattermost-redux/utils/channel_utils';
 import {makeGetChannelUnreadCount} from 'mattermost-redux/selectors/entities/channels';
 import {clearChannelSelection, multiSelectChannelAdd, multiSelectChannelTo} from 'actions/views/channel_sidebar';
-import {getFirstChannelName, getShowTutorialStep} from 'selectors/onboarding';
 import {markMostRecentPostInChannelAsUnread, unsetEditingPost} from 'actions/post_actions';
 import {isChannelSelected} from 'selectors/views/channel_sidebar';
 import {GlobalState} from 'types/store';
 import {Channel} from '@mattermost/types/channels';
-import {OnboardingTourSteps, TutorialTourName} from 'components/tours';
-import {OnboardingTasksName} from 'components/onboarding_tasks';
 
 import SidebarChannelLink from './sidebar_channel_link';
 
@@ -30,12 +27,6 @@ function makeMapStateToProps() {
     return (state: GlobalState, ownProps: OwnProps) => {
         const member = getMyChannelMemberships(state)[ownProps.channel.id];
         const unreadCount = getUnreadCount(state, ownProps.channel.id);
-        const firstChannelName = getFirstChannelName(state);
-        const showChannelsTutorialStep = getShowTutorialStep(state, {
-            tourName: TutorialTourName.ONBOARDING_TUTORIAL_STEP,
-            taskName: OnboardingTasksName.CHANNELS_TOUR,
-            tourStep: OnboardingTourSteps.CHANNELS,
-        });
 
         return {
             unreadMentions: unreadCount.mentions,
@@ -44,8 +35,6 @@ function makeMapStateToProps() {
             isMuted: isChannelMuted(member),
             hasUrgent: unreadCount.hasUrgent,
             isChannelSelected: isChannelSelected(state, ownProps.channel.id),
-            firstChannelName: showChannelsTutorialStep ? firstChannelName : '',
-            showChannelsTutorialStep,
         };
     };
 }
