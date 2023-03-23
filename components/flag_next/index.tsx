2e3627c2a41ae28a84af2c4e82ab44ae15eaf140
Léonard <leonard.jouve@infomaniak.com>
2023-03-23 10:01:05 +0100

    show next flag on mobile

--- components/flag_next/index.tsx
+++ components/flag_next/index.tsx
@@ -2,15 +2,21 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import {useSelector} from 'react-redux';
+
+import {getIsRhsMenuOpen} from 'selectors/rhs';
 
 import './flag_next.scss';
 
 const FlagNext = () => {
+    const isRhsOpen = useSelector(getIsRhsMenuOpen);
+
     if (document.cookie.indexOf('KCHAT_NEXT=always') === -1) {
         return null;
     }
+
     return (
-        <div className='flag-next'>
+        <div className={`flag-next ${isRhsOpen ? 'rhs-opened' : ''}`}>
             <p className='flag-next__text'>
                 {'NEXT'}
             </p>
