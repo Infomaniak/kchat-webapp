64c4e66cd58ee04d4fea9d0f4764d666296b1145
anton.buksa <anton.buksa@infomaniak.com>
2023-04-17 15:38:22 +0200

    show error details if error response contains a message

--- actions/file_actions.ts
+++ actions/file_actions.ts
@@ -108,7 +108,12 @@ export function uploadFile({file, name, type, rootId, channelId, clientId, onPro
                         channelId,
                         rootId,
                     });
-                    onError(localizeMessage('channel_loader.unknown_error', 'We received an unexpected status code from the server.') + ' (' + xhr.status + ')', clientId, channelId, rootId);
+                    const errorResponse = JSON.parse(xhr.response);
+                    if (errorResponse.message) {
+                        onError(errorResponse.message, clientId, channelId, rootId);
+                    } else {
+                        onError(localizeMessage('channel_loader.unknown_error', 'We received an unexpected status code from the server.') + ' (' + xhr.status + ')', clientId, channelId, rootId);
+                    }
                 }
             };
         }
