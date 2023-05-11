fc9d0c8c0f6fc512af21da47155cfa7ab476137f
Léonard <leonard.jouve@infomaniak.com>
2023-05-11 15:28:57 +0200

    init schedule post menu

--- components/advanced_text_editor/send_button/send_button.tsx
+++ components/advanced_text_editor/send_button/send_button.tsx
@@ -1,10 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {FormEvent, memo} from 'react';
+import React, {memo, useRef, useState} from 'react';
+import {useSelector} from 'react-redux';
 import {useIntl} from 'react-intl';
-import styled from 'styled-components';
-import {SendIcon} from '@infomaniak/compass-icons/components';
+import {Button, ButtonGroup, MenuItem, MenuList, Popover, styled} from '@mui/material';
+import {ChevronUpIcon, SendIcon} from '@infomaniak/compass-icons/components';
+
+import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+
+import CompassDesignProvider from 'components/compass_design_provider';
 
 import {t} from 'utils/i18n';
 
@@ -13,18 +18,20 @@ type SendButtonProps = {
     disabled: boolean;
 }
 
-const SendButtonContainer = styled.button`
+const SendButtonContainer = styled(Button)`
     display: flex;
     height: 32px;
     padding: 0 16px;
-    border: none;
     background: var(--button-bg);
-    border-radius: 4px;
     color: var(--button-color);
     cursor: pointer;
     place-content: center;
     place-items: center;
     transition: color 150ms;
+
+    :hover {
+        background-color: var(--button-bg);
+    }
 
     &--disabled,
     &[disabled] {
@@ -41,8 +48,22 @@ const SendButtonContainer = styled.button`
     }
 `;
 
+const SchedulePostButton = styled(SendButtonContainer)`
+    min-width: 26px !important;
+    padding: 0 5px;
+`;
+
+const StyledButtonGroup = styled(ButtonGroup)`
+    border-radius: 4px;
+`;
+
+const schedulePostItems = ['1', '2', '3'];
+
 const SendButton = ({disabled, handleSubmit}: SendButtonProps) => {
+    const theme = useSelector(getTheme);
     const {formatMessage} = useIntl();
+    const buttonGroupRef = useRef<HTMLDivElement>(null);
+    const [schedulePostMenuOpen, setSchedulePostMenuOpen] = useState(false);
 
     const sendMessage = (e: React.FormEvent) => {
         e.stopPropagation();
@@ -50,26 +71,71 @@ const SendButton = ({disabled, handleSubmit}: SendButtonProps) => {
         handleSubmit(e);
     };
 
+    const handleSchedulePost = (e: React.MouseEvent) => {
+        e.preventDefault();
+        setSchedulePostMenuOpen(true);
+    };
+
+    const handleClose = () => setSchedulePostMenuOpen(false);
+
+    const renderedScheduledPostItems = schedulePostItems.map((item) => <MenuItem key={item}>{item}</MenuItem>);
+
     return (
-        <SendButtonContainer
-            data-testid='SendMessageButton'
-            tabIndex={0}
-            aria-label={formatMessage({
-                id: 'create_post.send_message',
-                defaultMessage: 'Send a message',
-            })}
-            disabled={disabled}
-            onClick={sendMessage}
-        >
-            <SendIcon
-                size={18}
-                color='currentColor'
-                aria-label={formatMessage({
-                    id: t('create_post.icon'),
-                    defaultMessage: 'Create a post',
-                })}
-            />
-        </SendButtonContainer>
+        <CompassDesignProvider theme={theme}>
+            <StyledButtonGroup ref={buttonGroupRef}>
+                <SendButtonContainer
+                    data-testid='SendMessageButton'
+                    tabIndex={0}
+                    aria-label={formatMessage({
+                        id: 'create_post.send_message',
+                        defaultMessage: 'Send a message',
+                    })}
+                    disabled={disabled}
+                    onClick={sendMessage}
+                >
+                    <SendIcon
+                        size={18}
+                        color='currentColor'
+                        aria-label={formatMessage({
+                            id: t('create_post.icon'),
+                            defaultMessage: 'Create a post',
+                        })}
+                    />
+                </SendButtonContainer>
+
+                {/* start of schedule post component */}
+
+                <SchedulePostButton
+                    disabled={disabled}
+                    onClick={handleSchedulePost}
+                    aria-label={formatMessage({
+                        id: 'create_post.schedule_post',
+                        defaultMessage: 'Schedule post',
+                    })}
+                >
+                    <ChevronUpIcon size={16}/>
+                </SchedulePostButton>
+                <Popover
+                    anchorEl={buttonGroupRef.current}
+                    open={schedulePostMenuOpen}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                >
+                    <MenuList id='schedule-post-menu'>
+                        {renderedScheduledPostItems}
+                    </MenuList>
+                </Popover>
+
+                {/* end of schedule post component */}
+            </StyledButtonGroup>
+        </CompassDesignProvider>
     );
 };
 
