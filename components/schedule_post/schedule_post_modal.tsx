36f6a498cb295c27802da30b9245c989c1cac572
Léonard <leonard.jouve@infomaniak.com>
2023-05-12 13:36:22 +0200

    fix date time inputs size && schedule modal header

--- components/schedule_post/schedule_post_modal.tsx
+++ components/schedule_post/schedule_post_modal.tsx
@@ -3,6 +3,7 @@
 
 import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
+import {useIntl} from 'react-intl';
 import {Moment} from 'moment-timezone';
 
 import {schedulePost} from 'mattermost-redux/actions/posts';
@@ -23,17 +24,37 @@ type Props = {
 
 const SchedulePostModal = ({channelId, message, timestamp, timezone}: Props) => {
     const dispatch = useDispatch();
+    const {formatMessage} = useIntl();
     const [scheduleTimestamp, setScheduleTimestamp] = useState<Moment>(getRoundedTime(timestamp));
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
     const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
 
     const handleConfirm = () => dispatch(schedulePost(channelId, message, toUTCUnix(scheduleTimestamp.toDate())));
 
+    const modalHeaderText = (
+        <div>
+            <h1>
+                {formatMessage({
+                    id: 'create_post.schedule_post.modal.header',
+                    defaultMessage: 'Schedule draft',
+                })}
+            </h1>
+            {timezone && <span>{timezone}</span>}
+        </div>
+    );
+
+    const confirmButtonText = formatMessage({
+        id: 'create_post.schedule_post.modal.confirm',
+        defaultMessage: 'Schedule',
+    });
+
     const isConfirmDisabled = isMenuOpen || isDatePickerOpen;
 
     return (
         <GenericModal
             className='schedule-post-modal'
+            modalHeaderText={modalHeaderText}
+            confirmButtonText={confirmButtonText}
             isConfirmDisabled={isConfirmDisabled}
             handleConfirm={handleConfirm}
         >
