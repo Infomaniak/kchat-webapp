e9e3632c6f97cb3e40541250086bf9a18c37d892
Léonard <leonard.jouve@infomaniak.com>
2023-06-01 17:35:34 +0200

    temparary mock global drafts config

--- packages/mattermost-redux/src/reducers/entities/general.ts
+++ packages/mattermost-redux/src/reducers/entities/general.ts
@@ -10,7 +10,10 @@ import {ClientLicense, ClientConfig} from '@mattermost/types/config';
 function config(state: Partial<ClientConfig> = {}, action: GenericAction) {
     switch (action.type) {
     case GeneralTypes.CLIENT_CONFIG_RECEIVED:
-        return Object.assign({}, state, action.data);
+        return Object.assign({}, state, {
+            ...action.data,
+            FeatureFlagGlobalDrafts: 'true',
+        });
     case UserTypes.LOGIN: // Used by the mobile app
     case GeneralTypes.SET_CONFIG_AND_LICENSE:
         return Object.assign({}, state, action.data.config);
