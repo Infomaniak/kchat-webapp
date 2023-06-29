cc0d73c27be662ad901db0dbb73619ed5c03c3cd
Léonard <leonard.jouve@infomaniak.com>
2023-06-29 14:48:11 +0200

    only redirect in production environnement

--- packages/mattermost-redux/src/actions/users.ts
+++ packages/mattermost-redux/src/actions/users.ts
@@ -104,9 +104,7 @@ export function loadMeREST(): ActionFunc {
                 });
                 const lastKSuiteSeen = sortedSuites[0];
 
-                if (isDesktopApp()
-                // && process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'development'
-                ) { //eslint-disable-line no-process-env
+                if (isDesktopApp() && process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'development') { //eslint-disable-line no-process-env
                     window.postMessage({
                         type: 'switch-server',
                         data: lastKSuiteSeen.display_name,
