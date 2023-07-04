fede35cb97b07eddef855da66f0f48e982efccf5
anton.buksa <anton.buksa@infomaniak.com>
2023-07-04 15:36:17 +0200

    clean

--- packages/client/src/client4.ts
+++ packages/client/src/client4.ts
@@ -156,14 +156,16 @@ function isDesktopApp(): boolean {
 }
 
 const IKConstants = {
+
     // @ts-ignore
     LOGIN_URL: process.env.LOGIN_ENDPOINT, // eslint-disable-line no-process-env
     // @ts-ignore
     LOGOUT_URL: `${process.env.LOGIN_ENDPOINT}logout`, // eslint-disable-line no-process-env
     CLIENT_ID: 'A7376A6D-9A79-4B06-A837-7D92DB93965B',
+
     // @ts-ignore
     MANAGER_URL: process.env.MANAGER_ENDPOINT, // eslint-disable-line no-process-env
-}
+};
 
 const HEADER_AUTH = 'Authorization';
 const HEADER_BEARER = 'Bearer';
@@ -4304,12 +4306,13 @@ export default class Client4 {
             });
         }
 
-        if ((response.status === 401 && data?.result === 'redirect')) {
+        if (response.status === 401 && data?.result === 'redirect') {
+            // eslint-disable-next-line no-negated-condition
             if (!isDesktopApp()) {
                 window.location.href = data.uri;
-                return {data: true}
+            } else {
+                this.emitUserLoggedOutEvent!('/login', true, true);
             }
-            this.emitUserLoggedOutEvent!('/login', true, true);
         }
 
         if (headers.has(HEADER_X_VERSION_ID)) {
