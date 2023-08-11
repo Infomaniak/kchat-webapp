bc5749917d9eae85b40a5a033143a1bb9bae323a
Arthur Noguera <arthur.noguera@infomaniak.com>
2023-08-11 12:17:07 +0200

    fix: date
    Fixed date format with the on selector in the search input
    
    Changelog: fixed

--- components/suggestion/search_date_suggestion/search_date_suggestion.tsx
+++ components/suggestion/search_date_suggestion/search_date_suggestion.tsx
@@ -22,7 +22,7 @@ export default class SearchDateSuggestion extends Suggestion {
     };
 
     handleDayClick = (day: Date) => {
-        const dayString = day.toISOString().split('T')[0];
+        const dayString = new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate())).toISOString().split('T')[0];
         this.props.onClick(dayString, this.props.matchedPretext);
     }
 
