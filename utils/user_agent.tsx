5a9d14e3e48bb4a20f4f0a8d378404aaf1bead8a
anton.buksa <anton.buksa@infomaniak.com>
2023-08-28 12:02:08 +0200

    clean

--- utils/user_agent.tsx
+++ utils/user_agent.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {isServerVersionGreaterThanOrEqualTo} from './server_version';
-
 /*
 Example User Agents
 --------------------
