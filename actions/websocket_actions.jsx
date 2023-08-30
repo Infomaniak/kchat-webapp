ba2843ff6cba10702cf98fa4c6f59c266a81ff35
Benoit Nau <benoit.nau@infomaniak.com>
2023-08-30 15:04:36 +0200

    fix: reconnexion ws
    Force reconnection on the window online event & reload focus channel.
    
    Changelog: fixed

--- actions/websocket_actions.jsx
+++ actions/websocket_actions.jsx
@@ -104,7 +104,7 @@ import {getHistory} from 'utils/browser_history';
 import {loadChannelsForCurrentUser} from 'actions/channel_actions';
 import {loadCustomEmojisIfNeeded} from 'actions/emoji_actions';
 import {redirectUserToDefaultTeam} from 'actions/global_actions';
-import {handleNewPost} from 'actions/post_actions';
+import {handleNewPost, resetReloadPostsInChannel} from 'actions/post_actions';
 import * as StatusActions from 'actions/status_actions';
 import {loadProfilesForSidebar} from 'actions/user_actions';
 import store from 'stores/redux_store.jsx';
@@ -210,6 +210,10 @@ export function initialize() {
         token,
         currentChannelId,
     );
+
+    // handle immediate WS reconnection after loosing the network connection.
+    window.removeEventListener('online', reconnect);
+    window.addEventListener('online', reconnect);
 }
 
 export function close() {
@@ -286,6 +290,8 @@ export async function reconnect(socketId?: string) {
         // }
 
         dispatch(loadChannelsForCurrentUser());
+
+        dispatch(resetReloadPostsInChannel());
 
         if (mostRecentPost) {
             // eslint-disable-next-line no-console
