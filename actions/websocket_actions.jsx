06ffd5156a16736a516b55b7b08d24e4acc5fa33
anton.buksa <anton.buksa@infomaniak.com>
2023-10-09 15:08:22 +0200

    use event team_id instead of current

--- actions/websocket_actions.jsx
+++ actions/websocket_actions.jsx
@@ -1817,7 +1817,7 @@ function handleThreadFollowChanged(msg) {
         const state = doGetState();
         const thread = getThread(state, msg.data.thread_id);
         if (!thread && msg.data.state && msg.data.reply_count) {
-            await doDispatch(fetchThread(getCurrentUserId(state), getCurrentTeamId(state), msg.data.thread_id, true));
+            await doDispatch(fetchThread(getCurrentUserId(state), msg.data.team_id, msg.data.thread_id, true));
         }
         handleFollowChanged(doDispatch, msg.data.thread_id, msg.data.team_id, msg.data.state);
     };
