2eea6b89fd9b8bd554289a49de434b77eb12b4b3
Benoit Nau <benoit.nau@infomaniak.com>
2023-11-02 15:10:25 +0100

    activity_and_insights

---
+++ webapp/channels/src/components/activity_and_insights/insights/insights_title/insights_title.tsx
@@ -0,0 +1,78 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {memo, useCallback} from 'react';
+import {useIntl} from 'react-intl';
+
+import MenuWrapper from 'components/widgets/menu/menu_wrapper';
+import Menu from 'components/widgets/menu/menu';
+
+import * as Utils from 'utils/utils';
+import {InsightsScopes} from 'utils/constants';
+
+type Props = {
+    filterType: string;
+    setFilterTypeTeam: () => void;
+    setFilterTypeMy: () => void;
+}
+
+const InsightsTitle = (props: Props) => {
+    const {formatMessage} = useIntl();
+
+    const title = useCallback(() => {
+        if (props.filterType === InsightsScopes.TEAM) {
+            return (
+                formatMessage({
+                    id: 'insights.teamHeading',
+                    defaultMessage: 'Team insights',
+                })
+            );
+        }
+        return (
+            formatMessage({
+                id: 'insights.myHeading',
+                defaultMessage: 'My insights',
+            })
+        );
+    }, [props.filterType]);
+
+    return (
+        <MenuWrapper id='insightsFilterDropdown'>
+            <button className='insights-title'>
+                {title()}
+                <span className='icon'>
+                    <i className='icon icon-chevron-down'/>
+                </span>
+            </button>
+            <Menu
+                ariaLabel={Utils.localizeMessage('insights.filter.ariaLabel', 'Insights Filter Menu')}
+            >
+                <Menu.ItemAction
+                    id='insightsDropdownMy'
+                    buttonClass='insights-filter-btn'
+                    onClick={props.setFilterTypeMy}
+                    icon={
+                        <span className='icon'>
+                            <i className='icon icon-account-outline'/>
+                        </span>
+                    }
+                    text={Utils.localizeMessage('insights.filter.myInsights', 'My insights')}
+                />
+                <Menu.ItemAction
+                    id='insightsDropdownTeam'
+                    buttonClass={'insights-filter-btn'}
+                    onClick={props.setFilterTypeTeam}
+                    icon={
+                        <span className='icon'>
+                            <i className='icon icon-account-multiple-outline'/>
+                        </span>
+                    }
+                    text={Utils.localizeMessage('insights.filter.teamInsights', 'Team insights')}
+
+                />
+            </Menu>
+        </MenuWrapper>
+    );
+};
+
+export default memo(InsightsTitle);
