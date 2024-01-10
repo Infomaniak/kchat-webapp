4008abac29e1eadf2a21df79254f3c51e3ee4390
anton.buksa <anton.buksa@infomaniak.com>
2024-01-10 14:23:20 +0100

    type bridge

--- webapp/channels/src/types/store/index.ts
+++ webapp/channels/src/types/store/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import type {KSuiteBridge} from '@infomaniak/ksuite-bridge';
+
 import type {GlobalState as BaseGlobalState} from '@mattermost/types/store';
 
 import type {PluginsState} from './plugins';
@@ -14,6 +16,7 @@ export type DraggingState = {
 
 export type GlobalState = BaseGlobalState & {
     plugins: PluginsState;
+    ksuite_bridge: KSuiteBridge;
     storage: {
         storage: Record<string, any>;
         initialized: boolean;
