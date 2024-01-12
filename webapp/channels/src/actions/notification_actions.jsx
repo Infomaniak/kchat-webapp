115bb01004d72a2c670b1802299c6f10ed314831
Benoit Nau <benoit.nau@infomaniak.com>
2024-01-12 07:52:27 +0100

    Revert "css & clean code"
    This reverts commit 39f6b466577fac4ba87c95dae4c1c57e7b47e413.

--- webapp/channels/src/actions/notification_actions.jsx
+++ webapp/channels/src/actions/notification_actions.jsx
@@ -20,6 +20,7 @@ import {isThreadOpen} from 'selectors/views/threads';
 
 import {getHistory} from 'utils/browser_history';
 import Constants, {NotificationLevels, UserStatuses, IgnoreChannelMentions} from 'utils/constants';
+import DesktopApp from 'utils/desktop_api';
 import {t} from 'utils/i18n';
 import {stripMarkdown, formatWithRenderer} from 'utils/markdown';
 import MentionableRenderer from 'utils/markdown/mentionable_renderer';
