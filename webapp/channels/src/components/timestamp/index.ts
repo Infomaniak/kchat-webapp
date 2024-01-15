6c1739e67b8a3454fcb193e0933ed33afc3f3fde
Benoit Nau <benoit.nau@infomaniak.com>
2024-01-15 07:59:05 +0100

    Timestamp handler

--- webapp/channels/src/components/timestamp/index.ts
+++ webapp/channels/src/components/timestamp/index.ts
@@ -6,19 +6,16 @@ import {connect} from 'react-redux';
 import type {UserTimezone} from '@mattermost/types/users';
 
 import {getBool} from 'mattermost-redux/selectors/entities/preferences';
-import {makeGetUserTimezone} from 'mattermost-redux/selectors/entities/timezone';
-import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentTimezoneFull} from 'mattermost-redux/selectors/entities/timezone';
 import {getUserCurrentTimezone} from 'mattermost-redux/utils/timezone_utils';
-
-import {areTimezonesEnabledAndSupported} from 'selectors/general';
 
 import {Preferences} from 'utils/constants';
 
 import type {GlobalState} from 'types/store';
 
 import * as RelativeRanges from './relative_ranges';
-import type {Props as TimestampProps} from './timestamp';
 import Timestamp, {supportsHourCycle} from './timestamp';
+import type {Props as TimestampProps} from './timestamp';
 
 type Props = {
     userTimezone?: UserTimezone;
@@ -27,33 +24,23 @@ type Props = {
     hourCycle?: TimestampProps['hourCycle'];
 }
 
-export function makeMapStateToProps() {
-    const getUserTimezone = makeGetUserTimezone();
+export function mapStateToProps(state: GlobalState, ownProps: Props) {
+    const timeZone: TimestampProps['timeZone'] = getUserCurrentTimezone(ownProps.userTimezone ?? getCurrentTimezoneFull(state)) || undefined;
+    let hourCycle: TimestampProps['hourCycle'];
+    let hour12: TimestampProps['hour12'];
 
-    return (state: GlobalState, ownProps: Props) => {
-        const currentUserId = getCurrentUserId(state);
+    const useMilitaryTime = getBool(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.USE_MILITARY_TIME, false);
 
-        let timeZone: TimestampProps['timeZone'];
-        let hourCycle: TimestampProps['hourCycle'];
-        let hour12: TimestampProps['hour12'];
+    if (supportsHourCycle) {
+        hourCycle = ownProps.hourCycle || (useMilitaryTime ? 'h23' : 'h12');
+    } else {
+        hour12 = ownProps.hour12 ?? (!useMilitaryTime);
+    }
 
-        if (areTimezonesEnabledAndSupported(state)) {
-            timeZone = getUserCurrentTimezone(ownProps.userTimezone ?? getUserTimezone(state, currentUserId)) ?? undefined;
-        }
-
-        const useMilitaryTime = getBool(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.USE_MILITARY_TIME, false);
-
-        if (supportsHourCycle) {
-            hourCycle = ownProps.hourCycle || (useMilitaryTime ? 'h23' : 'h12');
-        } else {
-            hour12 = ownProps.hour12 ?? (!useMilitaryTime);
-        }
-
-        return {timeZone: ownProps.timeZone || timeZone, hourCycle, hour12};
-    };
+    return {timeZone: ownProps.timeZone || timeZone, hourCycle, hour12};
 }
 
-export default connect(makeMapStateToProps)(Timestamp);
+export default connect(mapStateToProps)(Timestamp);
 
 export {default as SemanticTime} from './semantic_time';
 export {RelativeRanges};
