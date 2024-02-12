b41d8139501edaa9e717f7df1ec310645b89eee6
Philippe Ka <philippe.karle@infomaniak.com>
2024-02-12 11:30:10 +0100

    fix: OnlyOffice preview component

--- webapp/channels/src/components/onlyoffice_preview.tsx
+++ webapp/channels/src/components/onlyoffice_preview.tsx
@@ -29,35 +29,63 @@ const connector = connect(undefined, mapDispatchToProps);
 export type PropsFromRedux = Props & ConnectedProps<typeof connector>;
 
 class OnlyofficePreview extends React.PureComponent<PropsFromRedux> {
-    async componentDidMount() {
-        if (document.getElementById('onlyoffice-script')) {
-            const result = await this.fetchFileInfoForUrl(this.props.fileInfo.id);
+    placeholderId: string;
+    scriptId: string;
+    scriptSrc: string;
+    container: React.RefObject<HTMLDivElement>;
 
-            if ('error' in result) {
-                console.warn(result.error);
-            } else {
-                this.loadDocument(result as FileInfo);
+    constructor(props: any) {
+        super(props);
+
+        this.placeholderId = 'onlyoffice-placeholder';
+        this.scriptId = 'onlyoffice-script';
+        this.scriptSrc = 'https://documentserver.kdrive.infomaniak.com/web-apps/apps/api/documents/api.js';
+        this.container = React.createRef();
+    }
+
+    async componentDidUpdate() {
+        await this.prepareDoc();
+    }
+
+    async componentDidMount() {
+        await this.prepareDoc();
+    }
+
+    async prepareDoc() {
+        if (document.getElementById(this.scriptId)) {
+            const fileInfo = await this.downloadFile(this.props.fileInfo.id);
+
+            if (fileInfo) {
+                this.loadDocument(fileInfo);
             }
         } else {
             const script = document.createElement('script');
-
             script.type = 'text/javascript';
-            script.id = 'onlyoffice-script';
-            script.src = 'https://documentserver.kdrive.infomaniak.com/web-apps/apps/api/documents/api.js';
+            script.id = this.scriptId;
+            script.src = this.scriptSrc;
             script.async = true;
 
             document.body.appendChild(script);
 
             script.onload = async () => {
-                const result = await this.fetchFileInfoForUrl(this.props.fileInfo.id);
+                const fileInfo = await this.downloadFile(this.props.fileInfo.id);
 
-                if ('error' in result) {
-                    console.warn(result.error);
-                } else {
-                    this.loadDocument(result as FileInfo);
+                if (fileInfo) {
+                    this.loadDocument(fileInfo);
                 }
             };
         }
+    }
+
+    async downloadFile(fileId: string) {
+        const result = await this.fetchFileInfoForUrl(fileId);
+
+        if ('error' in result) {
+            console.warn(result.error);
+            return null;
+        }
+
+        return result as FileInfo;
     }
 
     async fetchFileInfoForUrl(fileId: string) {
@@ -71,13 +99,26 @@ class OnlyofficePreview extends React.PureComponent<PropsFromRedux> {
     }
 
     loadDocument(freshFileInfo: FileInfo) {
-        // eslint-disable-next-line no-new
-        new DocsAPI.DocEditor('onlyoffice-placeholder', freshFileInfo.onlyoffice);
+        if (this.container.current) {
+            const placeholder = document.createElement('div');
+            placeholder.id = this.placeholderId;
+
+            this.container.current.innerHTML = '';
+            this.container.current.appendChild(placeholder);
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            // eslint-disable-next-line new-cap
+            DocsAPI.DocEditor(this.placeholderId, freshFileInfo.onlyoffice);
+        }
     }
 
     render() {
         return (
-            <div id='onlyoffice-placeholder'/>
+            <div
+                style={{height: '100%'}}
+                ref={this.container}
+            />
         );
     }
 }
