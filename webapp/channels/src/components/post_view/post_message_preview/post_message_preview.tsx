4eceba050d2ec9ece83dd2935383a8a2572bcc54
Philippe Ka <philippe.karle@infomaniak.com>
2024-03-28 12:23:11 +0100

    Shared message banner translation updated
    Changelog: changed

--- webapp/channels/src/components/post_view/post_message_preview/post_message_preview.tsx
+++ webapp/channels/src/components/post_view/post_message_preview/post_message_preview.tsx
@@ -144,7 +144,7 @@ const PostMessagePreview = (props: Props) => {
                 {previewFooterMessage || (
                     <FormattedMessage
                         id='post_message_preview.channel'
-                        defaultMessage='Only visible to users in ~{channel}'
+                        defaultMessage='In ~{channel}'
                         values={{
                             channel: channelDisplayName,
                         }}
