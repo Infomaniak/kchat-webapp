34734451b771b90c6af6007c7410297528f4804d
Philippe Ka <philippe.karle@infomaniak.com>
2024-04-22 17:49:04 +0200

    Fix: loading all users profiles available
    Changelog: fixed

--- webapp/channels/src/packages/mattermost-redux/src/actions/users.ts
+++ webapp/channels/src/packages/mattermost-redux/src/actions/users.ts
@@ -191,10 +191,22 @@ export function getFilteredUsersStats(options: GetFilteredUsersStatsOpts = {}, u
 
 export function getProfiles(page = 0, perPage: number = General.PROFILE_CHUNK_SIZE, options: any = {}): ActionFuncAsync<UserProfile[]> {
     return async (dispatch, getState) => {
-        let profiles: UserProfile[];
+        let profiles: UserProfile[] = [];
+        let currentFetch: UserProfile[];
+        let currentPage = page;
+
+        console.log('loading everybody');
 
         try {
-            profiles = await Client4.getProfiles(page, perPage, options);
+            while (true) {
+                // eslint-disable-next-line no-await-in-loop
+                currentFetch = await Client4.getProfiles(currentPage, perPage, options);
+                profiles = profiles.concat(currentFetch);
+                if (currentFetch.length < perPage) {
+                    break;
+                }
+                currentPage += 1;
+            }
         } catch (error) {
             forceLogoutIfNecessary(error, dispatch, getState);
             dispatch(logError(error));
