9a2e3e3fd2e92d37cee9165ac12126e3fd605ef7
anton.buksa <anton.buksa@infomaniak.com>
2024-05-21 10:54:06 +0200

    improve call config params

--- webapp/channels/src/utils/calls_utils.ts
+++ webapp/channels/src/utils/calls_utils.ts
@@ -6,5 +6,5 @@ export const isCallV3Available = () => {
 };
 
 export const openKmeetInExternalWindow = (baseUrl: string, jwt: string, subject: string) => {
-    window.open(baseUrl + `?jwt=${jwt}#config.localSubject="${subject}"#config.prejoinConfig.enabled=false&config.deeplinking.disabled=true&config.startWithVideoMuted=true`, '_blank', 'noopener');
+    window.open(baseUrl + `?jwt=${jwt}#config.disableInviteFunctions=true#config.localSubject="${encodeURIComponent(subject)}"#config.prejoinConfig.enabled=false&config.deeplinking.disabled=true&config.startWithVideoMuted=true`, '_blank', 'noopener');
 };
