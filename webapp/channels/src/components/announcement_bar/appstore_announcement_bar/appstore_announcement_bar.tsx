ae5f6b9567cde30d07b720a68492466a30c28351
Boris <Trombert>
2024-06-10 10:57:49 +0200

    Fix: Space errors and fix condition
    Changelog: fixed

--- webapp/channels/src/components/announcement_bar/appstore_announcement_bar/appstore_announcement_bar.tsx
+++ webapp/channels/src/components/announcement_bar/appstore_announcement_bar/appstore_announcement_bar.tsx
@@ -19,51 +19,54 @@ import AnnouncementBar from '../default_announcement_bar';
 interface Props {
     config?: Partial<ClientConfig>;
 }
-export default class AppStoreBar extends React.PureComponent <Props> {
+export default class AppStoreBar extends React.PureComponent<Props> {
     render() {
         const {config} = this.props;
 
-        const latestVersion = config!.MASLatestVersion!;
+        const latestVersion = config?.MASLatestVersion || '';
         const userAgentVersion = UserAgent.getDesktopVersion();
 
-        if (userAgentVersion && isServerVersionGreaterThanOrEqualTo(latestVersion, userAgentVersion) && userAgentVersion !== latestVersion && latestVersion) {
-            return (
-                <AnnouncementBar
-                    type={AnnouncementBarTypes.UPDATE_MAC}
-                    icon={
-                        <img
-                            className='version-alert-icon'
-                            src={informationIcon}
-                        />
-                    }
-                    message={
-                        <React.Fragment>
-                            <div style={{fontWeight: 400}}>
-
-                                <FormattedMessage
-                                    id='appstore.bar'
-                                    defaultMessage='A new version of the kChat app is available. Please'
-                                />
-                                <a
-                                    style={{marginLeft: '.5rem'}}
-                                    href='https://apps.apple.com/fr/app/infomaniak-kchat/id6443845553'
-                                >
-                                    <FormattedMessage
-                                        id='appstore.bar.update'
-                                        defaultMessage='update'
-                                    />
-                                </a>
-                                <FormattedMessage
-                                    id='appstore.bar.end'
-                                    defaultMessage=' from the App Store to benefit from the latest improvements'
-                                />
-                                {'.'}
-                            </div>
-                        </React.Fragment>
-                    }
-                />
-            );
+        if (!latestVersion || !userAgentVersion) {
+            return null;
         }
-        return null;
+        if (isServerVersionGreaterThanOrEqualTo(userAgentVersion, latestVersion)) {
+            return null;
+        }
+        return (
+            <AnnouncementBar
+                type={AnnouncementBarTypes.UPDATE_MAC}
+                icon={
+                    <img
+                        className='version-alert-icon'
+                        src={informationIcon}
+                    />
+                }
+                message={
+                    <React.Fragment>
+                        <div style={{fontWeight: 400}}>
+
+                            <FormattedMessage
+                                id='appstore.bar'
+                                defaultMessage='A new version of the kChat app is available. Please'
+                            />
+                            <a
+                                style={{marginLeft: '.5rem'}}
+                                href='https://apps.apple.com/fr/app/infomaniak-kchat/id6443845553'
+                            >
+                                <FormattedMessage
+                                    id='appstore.bar.update'
+                                    defaultMessage='update'
+                                />
+                            </a>
+                            <FormattedMessage
+                                id='appstore.bar.end'
+                                defaultMessage=' from the App Store to benefit from the latest improvements'
+                            />
+                            {'.'}
+                        </div>
+                    </React.Fragment>
+                }
+            />
+        );
     }
 }
