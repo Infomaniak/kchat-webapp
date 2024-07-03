d488f2f23d470252b4beea4580951cabd7ad7a2f
anton.buksa <anton.buksa@infomaniak.com>
2024-07-03 18:04:33 +0200

    disable fetching channel own member on posted event
    Changelog: changed

--- webapp/channels/src/actions/post_actions.ts
+++ webapp/channels/src/actions/post_actions.ts
@@ -52,21 +52,22 @@ import type {NewPostMessageProps} from './new_post';
 const ReactionLimitReachedModal = withSuspense(lazy(() => import('components/reaction_limit_reached_modal')));
 
 export function handleNewPost(post: Post, msg?: {data?: NewPostMessageProps & GroupChannel}): ActionFuncAsync<boolean, GlobalState> {
-    return async (dispatch, getState) => {
+    return async (dispatch) => {
         let websocketMessageProps = {};
-        const state = getState();
+
+        // const state = getState();
         if (msg) {
             websocketMessageProps = msg.data!;
         }
 
-        const myChannelMember = getMyChannelMemberSelector(state, post.channel_id);
-        const myChannelMemberDoesntExist = !myChannelMember || (Object.keys(myChannelMember).length === 0 && myChannelMember.constructor === Object);
+        // const myChannelMember = getMyChannelMemberSelector(state, post.channel_id);
+        // const myChannelMemberDoesntExist = !myChannelMember || (Object.keys(myChannelMember).length === 0 && myChannelMember.constructor === Object);
 
-        if (myChannelMemberDoesntExist) {
-            await dispatch(getMyChannelMember(post.channel_id));
-        }
+        // if (myChannelMemberDoesntExist) {
+        //     await dispatch(getMyChannelMember(post.channel_id));
+        // }
 
-        dispatch(completePostReceive(post, websocketMessageProps as NewPostMessageProps, myChannelMemberDoesntExist));
+        dispatch(completePostReceive(post, websocketMessageProps as NewPostMessageProps, false));
 
         if (msg && msg.data) {
             if (msg.data.channel_type === Constants.DM_CHANNEL) {
