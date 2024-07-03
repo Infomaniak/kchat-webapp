a0354eb43f4f02b3b85b6889bc8a04fb11918385
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-07-03 10:02:12 +0200

    chore(e2e): Updated playwright to 1.43.1

--- e2e-tests/.ci/server.generate.sh
+++ e2e-tests/.ci/server.generate.sh
@@ -221,7 +221,7 @@ $(if mme2e_is_token_in_list "webhook-interactions" "$ENABLED_DOCKER_SERVICES"; t
 $(if mme2e_is_token_in_list "playwright" "$ENABLED_DOCKER_SERVICES"; then
     echo '
   playwright:
-    image: mcr.microsoft.com/playwright:v1.41.1-jammy
+    image: mcr.microsoft.com/playwright:v1.43.1-jammy
     entrypoint: ["/bin/bash", "-c"]
     command: ["until [ -f /var/run/mm_terminate ]; do sleep 5; done"]
     env_file:
