d6a727a9ac38eb4f4de4ee9854d0364b5c11fb16
Ovgodd <c.gromoff@gmail.com>
2024-07-25 10:20:06 +0200

    change timer

--- webapp/channels/src/components/advanced_create_comment/advanced_create_comment.tsx
+++ webapp/channels/src/components/advanced_create_comment/advanced_create_comment.tsx
@@ -787,7 +787,7 @@ class AdvancedCreateComment extends React.PureComponent<Props, State> {
     };
 
     emitRecordingEvent = () => {
-        const TIMER = 1000;
+        const TIMER = 500;
         this.recordingInterval = setInterval(() => {
             const {channelId, rootId} = this.props;
             GlobalActions.emitLocalUserTypingEvent('recording', channelId, rootId);
--- webapp/channels/src/components/advanced_create_post/advanced_create_post.tsx
+++ webapp/channels/src/components/advanced_create_post/advanced_create_post.tsx
@@ -922,7 +922,7 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
     };
 
     emitRecordingEvent = () => {
-        const TIMER = 10;
+        const TIMER = 500;
         this.recordingInterval = setInterval(() => {
             const channelId = this.props.currentChannel.id;
             GlobalActions.emitLocalUserTypingEvent('recording', channelId, '');
