78d809a35671b3f43905bb8529f400cdb8bf76ed
anton.buksa <anton.buksa@infomaniak.com>
2024-10-28 15:32:55 +0100

    redirect to kchat tab directly in ksuite redirect
    Changelog: fixed

--- webapp/channels/src/packages/mattermost-redux/src/actions/users.ts
+++ webapp/channels/src/packages/mattermost-redux/src/actions/users.ts
@@ -79,7 +79,7 @@ export function loadMe(): ActionFuncAsync<boolean> {
 
         if (!bridge.isConnected && !isDesktopApp() && Client4.isIkBaseUrl() && process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'development') { //eslint-disable-line no-process-env
             // eslint-disable-next-line no-process-env
-            window.location.assign(`https://ksuite.${process.env.BASE_URL?.split('kchat.')[1]}`);
+            window.location.assign(`https://ksuite.${process.env.BASE_URL?.split('kchat.')[1]}/kchat`);
 
             return;
         }
