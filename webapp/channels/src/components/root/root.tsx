09a1851688410da03656ffa0b26f97cddaea8047
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-04-01 15:15:26 +0200

    add some log

--- webapp/channels/src/components/root/root.tsx
+++ webapp/channels/src/components/root/root.tsx
@@ -613,7 +613,7 @@ export default class Root extends React.PureComponent<Props, State> {
         // this message listener is outside the store because of how is handled navigation
         ksuiteBridge.on(NavigateMessageKey, (navigateMessage: NavigateMessage) => {
             confirm("test?")
-            console.log("j'ai testé")
+            console.log("navigate to", navigateMessage)
             getHistory().push(navigateMessage.path);
         });
 
