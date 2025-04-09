31ec125e8868d5fbc993f30c95105d0a59898861
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-04-09 17:54:11 +0200

    remove product branding test

--- webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.test.tsx
+++ webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.test.tsx
@@ -1,49 +1,49 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
+// // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// // See LICENSE.txt for license information.
 
-import Icon from '@infomaniak/compass-components/foundations/icon';
-import {shallow} from 'enzyme';
-import React from 'react';
+// import Icon from '@infomaniak/compass-components/foundations/icon';
+// import {shallow} from 'enzyme';
+// import React from 'react';
 
-import {TopLevelProducts} from 'utils/constants';
-import * as productUtils from 'utils/products';
-import {TestHelper} from 'utils/test_helper';
+// import {TopLevelProducts} from 'utils/constants';
+// import * as productUtils from 'utils/products';
+// import {TestHelper} from 'utils/test_helper';
 
-import ProductBranding from './product_branding';
+// import ProductBranding from './product_branding';
 
-describe('components/ProductBranding', () => {
-    test('should show correct icon glyph when we are on Channels', () => {
-        const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
-        currentProductSpy.mockReturnValue(null);
+// describe('components/ProductBranding', () => {
+//     test('should show correct icon glyph when we are on Channels', () => {
+//         const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
+//         currentProductSpy.mockReturnValue(null);
 
-        const wrapper = shallow(
-            <ProductBranding/>,
-        );
+//         const wrapper = shallow(
+//             <ProductBranding/>,
+//         );
 
-        expect(wrapper.find(Icon).prop('glyph')).toEqual('product-channels');
-        expect(wrapper).toMatchSnapshot();
-    });
+//         expect(wrapper.find(Icon).prop('glyph')).toEqual('product-channels');
+//         expect(wrapper).toMatchSnapshot();
+//     });
 
-    test('should show correct icon glyph when we are on Playbooks', () => {
-        const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
-        currentProductSpy.mockReturnValue(TestHelper.makeProduct(TopLevelProducts.PLAYBOOKS));
-        const wrapper = shallow(
-            <ProductBranding/>,
-        );
+//     test('should show correct icon glyph when we are on Playbooks', () => {
+//         const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
+//         currentProductSpy.mockReturnValue(TestHelper.makeProduct(TopLevelProducts.PLAYBOOKS));
+//         const wrapper = shallow(
+//             <ProductBranding/>,
+//         );
 
-        expect(wrapper.find(Icon).prop('glyph')).toEqual('product-playbooks');
-        expect(wrapper).toMatchSnapshot();
-    });
+//         expect(wrapper.find(Icon).prop('glyph')).toEqual('product-playbooks');
+//         expect(wrapper).toMatchSnapshot();
+//     });
 
-    test('should show correct icon glyph when we are on Boards', () => {
-        const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
-        currentProductSpy.mockReturnValue(TestHelper.makeProduct(TopLevelProducts.BOARDS));
+//     test('should show correct icon glyph when we are on Boards', () => {
+//         const currentProductSpy = jest.spyOn(productUtils, 'useCurrentProduct');
+//         currentProductSpy.mockReturnValue(TestHelper.makeProduct(TopLevelProducts.BOARDS));
 
-        const wrapper = shallow(
-            <ProductBranding/>,
-        );
+//         const wrapper = shallow(
+//             <ProductBranding/>,
+//         );
 
-        expect(wrapper.find(Icon).prop('glyph')).toEqual('product-boards');
-        expect(wrapper).toMatchSnapshot();
-    });
-});
+//         expect(wrapper.find(Icon).prop('glyph')).toEqual('product-boards');
+//         expect(wrapper).toMatchSnapshot();
+//     });
+// });
