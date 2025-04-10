9b442e4ce63f9c3175d3badbd875316a8b652f78
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-04-10 17:02:22 +0200

    fix post_profile_picture.test.tsx

--- webapp/channels/src/components/post_profile_picture/post_profile_picture.test.tsx
+++ webapp/channels/src/components/post_profile_picture/post_profile_picture.test.tsx
@@ -1,12 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type {ComponentProps} from 'react';
 import React from 'react';
+import type {ComponentProps} from 'react';
 
 import {TestHelper} from 'utils/test_helper';
 
-import {renderWithContext} from 'tests/react_testing_utils';
+import {renderWithContext, screen} from 'tests/react_testing_utils';
 
 import PostProfilePicture from './post_profile_picture';
 
@@ -30,13 +30,16 @@ describe('components/PostProfilePicture', () => {
         isBot: Boolean(user.is_bot),
     };
 
-    test('should match snapshot, no status and post icon override specified, default props', () => {
+    test('no status and post icon override specified, default props', () => {
         const props: Props = baseProps;
         renderWithContext(
             <PostProfilePicture {...props}/>,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        expect(screen.queryByLabelText('Online Icon')).not.toBeInTheDocument();
+
+        // no status is given, 'Offline Icon' should be in the dom as a fallback
+        expect(screen.getByLabelText('Offline Icon')).toBeInTheDocument();
     });
 
     test('status is specified, default props', () => {
@@ -48,6 +51,13 @@ describe('components/PostProfilePicture', () => {
             <PostProfilePicture {...props}/>,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        // status is given, 'Away Icon' should be in the dom
+        expect(screen.getByLabelText('Away Icon')).toBeInTheDocument();
+
+        expect(screen.queryByLabelText('Online Icon')).not.toBeInTheDocument();
+
+        expect(screen.queryByLabelText('Offline Icon')).not.toBeInTheDocument();
+
+        expect(screen.getAllByRole('img')).toHaveLength(2);
     });
 });
