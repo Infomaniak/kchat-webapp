a1cbadc21061cdee2d84ef09b3219a7eae3b4081
Boris Trombert <boris.trombert@infomaniak.com>
2025-04-29 17:07:59 +0200

    Revert "fix: test remove unused desktop api"
    This reverts commit cc558115252c8c127bfb92267fc5d894f29808c0.

--- webapp/channels/src/utils/desktop_api.ts
+++ webapp/channels/src/utils/desktop_api.ts
@@ -271,9 +271,9 @@ export class DesktopAppAPI {
     updateUnreadsAndMentions = (isUnread: boolean, mentionCount: number) =>
         window.desktopAPI?.setUnreadsAndMentions && window.desktopAPI.setUnreadsAndMentions(isUnread, mentionCount);
     setSessionExpired = (expired: boolean) => window.desktopAPI?.setSessionExpired && window.desktopAPI.setSessionExpired(expired);
-    signalLogin = () => {};
-    signalLogout = () => {};
-    reactAppInitialized = () => {};
+    signalLogin = () => window.desktopAPI?.onLogin?.();
+    signalLogout = () => window.desktopAPI?.onLogout?.();
+    reactAppInitialized = () => window.desktopAPI?.reactAppInitialized?.();
 
     /*********************************************************************
      * Helper functions for legacy code
