04edabdc9fc2016bc816a07ff2fcdea60a077332
Shaï Halimi <shai.halimi@infomaniak.com>
2025-05-06 16:23:43 +0200

    feat: removed old profile popover and used WC

--- webapp/channels/src/components/profile_popover/index.ts
+++ webapp/channels/src/components/profile_popover/index.ts
@@ -2,5 +2,37 @@
 // See LICENSE.txt for license information.
 
 import {ProfilePopoverController} from './profile_popover_controller';
+import type {GlobalState} from "../../types/store";
+import {getCurrentUser} from "mattermost-redux/selectors/entities/users";
+import {bindActionCreators, Dispatch} from "redux";
+import {startOrJoinCallInChannelV2} from "../../actions/calls";
+import {joinCall} from "../../actions/kmeet_calls";
+import {closeModal, openModal} from "../../actions/views/modals";
+import {connect} from "react-redux";
+import {getCurrentTeamAccountId, getCurrentTeamName} from "mattermost-redux/selectors/entities/teams";
 
-export default ProfilePopoverController;
+
+function mapStateToProps(state: GlobalState) {
+    const currentUser = getCurrentUser(state);
+    const currentTeamAccountId = getCurrentTeamAccountId(state);
+    const currentTeamName = getCurrentTeamName(state);
+
+    return {
+        currentUser,
+        currentTeamAccountId,
+        currentTeamName,
+    };
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        actions: bindActionCreators({
+            startOrJoinCallInChannelV2,
+            joinCall,
+            openModal,
+            closeModal,
+        }, dispatch),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePopoverController);
