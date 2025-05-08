fcd0644a2420c43a33982ef8665c131856ab96ab
Shaï Halimi <shai.halimi@infomaniak.com>
2025-05-08 09:11:49 +0200

    feat: crwated mail attachement type and render function

--- webapp/channels/src/components/post_markdown/system_message_helpers.tsx
+++ webapp/channels/src/components/post_markdown/system_message_helpers.tsx
@@ -22,6 +22,7 @@ import PostNotifyChannelMember from 'components/post_view/post_notify_channel_me
 import VoiceMessageAttachmentPlayer from 'components/voice/post_type';
 
 import {isChannelNamesMap, type TextFormattingOptions} from 'utils/text_formatting';
+import {MailAttachmentMessage} from "../mail_attachement/mail_attachment";
 
 export function renderUsername(value: unknown): ReactNode {
     const verifiedValue = ensureString(value);
@@ -373,6 +374,12 @@ function renderCallNotificationMessage(post: Post): ReactNode {
     );
 }
 
+function renderMailAttachmentMessage(post: Post): ReactNode {
+    return (
+        <MailAttachmentMessage post={post}/>
+    );
+}
+
 function renderVoiceMessage(post: Post): ReactNode {
     return (
         <VoiceMessageAttachmentPlayer post={post}/>
@@ -419,6 +426,7 @@ const systemMessageRenderers = {
     [Posts.POST_TYPES.CHANNEL_UNARCHIVED]: renderChannelUnarchivedMessage,
     [Posts.POST_TYPES.ME]: renderMeMessage,
     [Posts.POST_TYPES.CALL]: renderCallNotificationMessage,
+    [Posts.POST_TYPES.MAIL_ATTACHMENT]: renderMailAttachmentMessage,
     [Posts.POST_TYPES.VOICE]: renderVoiceMessage,
     [Posts.POST_TYPES.SYSTEM_POST_REMINDER]: renderReminderSystemBotMessage,
     [Posts.POST_TYPES.SYSTEM_WELCOME_MESSAGE]: () => <></>, // Infomaniak: return fragment to avoid displaying message from backend
