44f75b0d49c55838595cf2337cd30bc823c3ff0a
Shaï Halimi <shai.halimi@infomaniak.com>
2025-05-13 12:38:01 +0200

    chore: reset stylesheet and used new one to avoid futur conflict

--- webapp/channels/src/components/profile_popover/profile_popover_wc_controller.tsx
+++ webapp/channels/src/components/profile_popover/profile_popover_wc_controller.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import './profile_popover.scss';
+import './wc_profile_popover.scss';
 import type {
     ReactNode,
 } from 'react';
