1df8056b45c059d6aa06e142006c7a8bf5d1aa12
Shaï Halimi <shai.halimi@infomaniak.com>
2025-05-13 09:12:19 +0200

    chore: re-set everthings as close as possible to merge upstream

--- webapp/channels/src/components/profile_popover/index.ts
+++ webapp/channels/src/components/profile_popover/index.ts
@@ -3,18 +3,20 @@
 
 import {connect} from 'react-redux';
 
+import type {UserProfile} from '@mattermost/types/users';
+
 import {getCurrentTeamAccountId, getCurrentTeamName} from 'mattermost-redux/selectors/entities/teams';
 import {getCurrentUser, getStatusForUserId, getUser as selectUser} from 'mattermost-redux/selectors/entities/users';
 
+import type {
+    ProfilePopoverAdditionalProps,
+    ProfilePopoverProps} from './profile_popover_wc_controller';
+import {
+    ProfilePopoverWcController,
+} from './profile_popover_wc_controller';
 
 import type {GlobalState} from '../../types/store';
 import {UserStatuses} from '../../utils/constants';
-import {
-    ProfilePopoverAdditionalProps,
-    ProfilePopoverProps,
-    ProfilePopoverWcController
-} from "./profile_popover_wc_controller";
-import type {UserProfile} from "@mattermost/types/users";
 
 function mapStateToProps(state: GlobalState, ownProps: ProfilePopoverProps): ProfilePopoverAdditionalProps {
     const currentUser = getCurrentUser(state);
@@ -23,13 +25,12 @@ function mapStateToProps(state: GlobalState, ownProps: ProfilePopoverProps): Pro
     const user = selectUser(state, ownProps.userId) as UserProfile | undefined;
     const userStatus = (ownProps.userId && getStatusForUserId(state, ownProps.userId)) || UserStatuses.OFFLINE;
 
-
     return {
         currentTeamAccountId,
         currentTeamName,
         currentUser,
         user,
-        userStatus
+        userStatus,
     };
 }
 
