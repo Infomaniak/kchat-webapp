3f3a44db22148632409674d314a35e3c852f7ace
Shaï Halimi <shai.halimi@infomaniak.com>
2025-05-13 09:14:25 +0200

    chore: add classes

--- webapp/channels/src/components/at_mention/at_mention.tsx
+++ webapp/channels/src/components/at_mention/at_mention.tsx
@@ -60,6 +60,7 @@ const AtMention = (props: Props) => {
         return (
             <>
                 <ProfilePopover
+                    triggerComponentClass={classNames('style--none', {'mention--highlight': highlightMention})}
                     userId={user.id}
                     src={Client4.getProfilePictureUrl(user.id, user.last_picture_update)}
                     channelId={props.channelId}
