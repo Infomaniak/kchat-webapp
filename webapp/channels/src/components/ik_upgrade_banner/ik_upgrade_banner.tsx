beaf198712b5c43c19a99392882077a2ff6895f0
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-06-18 15:00:10 +0200

    Don't show banner for standard pack

--- webapp/channels/src/components/ik_upgrade_banner/ik_upgrade_banner.tsx
+++ webapp/channels/src/components/ik_upgrade_banner/ik_upgrade_banner.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './ik_upgrade_banner.css';
 
-//TODO: handle the size of the wc. FOr now it will be used withing the channelIntro which is not full wide
-export default function UpgradeBanner() {
+import type {PackName} from '@mattermost/types/teams';
+
+import type {PropsFromRedux} from './index';
+
+const packNameThatShowsUpgrade: PackName[] = ['ksuite_essential'];
+export default function UpgradeBanner({packName}: PropsFromRedux) {
+    const shouldHideBanner = !packName || !packNameThatShowsUpgrade.includes(packName);
+    if (shouldHideBanner) {
+        return null;
+    }
+
+    //TODO: use the correct plan to setup the message correctly
     return (
         <div className='upgrade-banner-placeholder'>
             Placeholder WC-BANNER
