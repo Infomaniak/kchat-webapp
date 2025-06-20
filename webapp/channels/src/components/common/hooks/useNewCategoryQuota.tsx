f242c941017838f2c55601b2fdf5a123b713b716
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-06-20 10:18:41 +0200

    protect new category upsell with pack limitation

--- webapp/channels/src/components/common/hooks/useNewCategoryQuota.tsx
+++ webapp/channels/src/components/common/hooks/useNewCategoryQuota.tsx
@@ -1,4 +1,7 @@
 import React, {useRef} from 'react';
+import {useSelector} from 'react-redux';
+
+import {getCurrentPackName} from 'mattermost-redux/selectors/entities/teams';
 
 import useGetUsageDeltas from './useGetUsageDeltas';
 
@@ -8,15 +11,18 @@ export function useCategoryCreationWithLimitation(props: {onClick: () => void}) 
     const openUpgradePopupOpen = () => {
         modalRef.current?.open();
     };
-
+    const packaName = useSelector(getCurrentPackName);
     const {sidebar_categories: remainingCategoryNegative} = useGetUsageDeltas();
-    const available = remainingCategoryNegative < 0;
+    const available = remainingCategoryNegative < 0 && packaName === 'ksuite_essential';
     const onClick = available ? props.onClick : openUpgradePopupOpen;
 
     let OptionalUpgradeButton = null;
     if (!available) {
         OptionalUpgradeButton = (
-            <wc-ksuite-modal-conversion ref={modalRef}>
+            <wc-ksuite-modal-conversion
+                ref={modalRef}
+                modalType='standard'
+            >
                 <wc-modal-conversion-tag/>
             </wc-ksuite-modal-conversion>
         );
