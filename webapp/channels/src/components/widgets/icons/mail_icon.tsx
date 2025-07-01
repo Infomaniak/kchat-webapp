3c0c46344af6b3f47dd92623af6727023e9fb886
Boris Trombert <boris.trombert@infomaniak.com>
2025-07-01 17:24:23 +0200

    fix: trying something

--- webapp/channels/src/components/widgets/icons/mail_icon.tsx
+++ webapp/channels/src/components/widgets/icons/mail_icon.tsx
@@ -7,11 +7,8 @@ import {useIntl} from 'react-intl';
 export default function MailIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
-        <span
-            {...props}
-
-        > <svg
-
+        <span {...props}>
+            <svg
                 width='24px'
                 height='24px'
                 viewBox='0 0 24 24'
@@ -20,7 +17,6 @@ export default function MailIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
             >
                 <path d='M4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4C2.89,20 2,19.1 2,18V6C2,4.89 2.89,4 4,4M12,11L20,6H4L12,11M4,18H20V8.37L12,13.36L4,8.37V18Z'/>
             </svg>
-
         </span>
     );
 }
