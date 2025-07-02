5743c43d933a66b08034cfc66a6a57a43bae1f60
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-07-02 11:00:22 +0200

    add test to check plural / singular

--- webapp/channels/src/packages/mattermost-redux/src/utils/duration.test.ts
+++ webapp/channels/src/packages/mattermost-redux/src/utils/duration.test.ts
@@ -34,3 +34,48 @@ describe('formatYMDDurationHuman', () => {
         expect(formatYMDDurationHuman('P1Y2M3DT4H5M6S', 'en')).toBe('1 year 2 months 3 days');
     });
 });
+
+describe('formatYMDDurationHuman - singular/plural', () => {
+    const cases = [
+        ['P1Y', 'en', '1 year'],
+        ['P2Y', 'en', '2 years'],
+        ['P1M', 'en', '1 month'],
+        ['P3M', 'en', '3 months'],
+        ['P1D', 'en', '1 day'],
+        ['P5D', 'en', '5 days'],
+
+        ['P1Y', 'fr', '1 an'],
+        ['P2Y', 'fr', '2 ans'],
+        ['P1M', 'fr', '1 mois'],
+        ['P3M', 'fr', '3 mois'],
+        ['P1D', 'fr', '1 jour'],
+        ['P4D', 'fr', '4 jours'],
+
+        ['P1Y', 'es', '1 año'],
+        ['P2Y', 'es', '2 años'],
+        ['P1M', 'es', '1 mes'],
+        ['P2M', 'es', '2 meses'],
+        ['P1D', 'es', '1 día'],
+        ['P7D', 'es', '7 días'],
+
+        ['P1Y', 'de', '1 Jahr'],
+        ['P2Y', 'de', '2 Jahre'],
+        ['P1M', 'de', '1 Monat'],
+        ['P3M', 'de', '3 Monate'],
+        ['P1D', 'de', '1 Tag'],
+        ['P9D', 'de', '9 Tage'],
+
+        ['P1Y', 'it', '1 anno'],
+        ['P2Y', 'it', '2 anni'],
+        ['P1M', 'it', '1 mese'],
+        ['P3M', 'it', '3 mesi'],
+        ['P1D', 'it', '1 giorno'],
+        ['P8D', 'it', '8 giorni'],
+    ];
+
+    cases.forEach(([iso, locale, expected]) => {
+        it(`formats ${iso} in ${locale} as "${expected}"`, () => {
+            expect(formatYMDDurationHuman(iso as string, locale as string)).toBe(expected);
+        });
+    });
+});
