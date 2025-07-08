53eaf3eeee4f1fb88d91878c7123e0d1b876d0b4
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-07-08 09:17:20 +0200

    Update CSP to allow staging

--- webapp/channels/webpack.config.js
+++ webapp/channels/webpack.config.js
@@ -341,7 +341,7 @@ var config = {
 };
 
 function generateCSP() {
-    let csp = 'script-src \'self\' blob: cdn.rudderlabs.com/ js.stripe.com/v3 web-components.storage.infomaniak.com/ web-components-staging.dev.infomaniak.ch/ welcome.infomaniak.com/ welcome.preprod.dev.infomaniak.ch/ kmeet.infomaniak.com/ kmeet.preprod.dev.infomaniak.ch/ onlyoffice.infomaniak.com/ ' + CSP_UNSAFE_INLINE + CSP_UNSAFE_EVAL_IF_DEV;
+    let csp = "script-src 'self' blob: cdn.rudderlabs.com/ js.stripe.com/v3 web-components.storage.infomaniak.com/ web-components-staging.dev.infomaniak.ch/ welcome.infomaniak.com/ welcome.preprod.dev.infomaniak.ch/ kmeet.infomaniak.com/ kmeet.preprod.dev.infomaniak.ch/ onlyoffice.infomaniak.com" + CSP_UNSAFE_INLINE + CSP_UNSAFE_EVAL_IF_DEV + "; script-src-elem 'self' 'unsafe-inline' blob: kchat.preprod.dev.infomaniak.ch kmeet.preprod.dev.infomaniak.ch web-components.storage.infomaniak.com documentserver.kdrive.infomaniak.com onlyoffice.infomaniak.com web-components-staging.dev.infomaniak.ch;";
 
     if (IS_CANARY || IS_PREPROD) {
         csp += CSP_WORKER_SRC;
