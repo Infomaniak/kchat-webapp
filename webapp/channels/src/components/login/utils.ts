bc1b936de5d481f67d922f37ed7743c8e71d5ba1
Boris Trombert <boris.trombert@infomaniak.com>
2025-07-11 13:29:34 +0200

    fix: revert error push

--- webapp/channels/src/components/login/utils.ts
+++ webapp/channels/src/components/login/utils.ts
@@ -106,11 +106,7 @@ export function getChallengeAndRedirectToLogin(infinite = false) {
         // TODO: store in redux instead of localstorage
         localStorage.setItem('challenge', JSON.stringify({verifier: codeVerifier, challenge: codeChallenge}));
 
-        // window.location.assign(`https://login.preprod.dev.infomaniak.ch/authorize?code_challenge=${codeChallenge}${infinite ? '' : '&access_type=offline'}&code_challenge_method=S256&client_id=${IKConstants.CLIENT_ID}&response_type=code&redirect_uri=https://infomaniak.kchat.preprod.dev.infomaniak.ch/`);
-        // console.log('test', `https://login.preprod.dev.infomaniak.ch/authorize?code_challenge=${codeChallenge}${infinite ? '' : '&access_type=offline'}&code_challenge_method=S256&client_id=${IKConstants.CLIENT_ID}&response_type=code&redirect_uri=https://infomaniak.kchat.preprod.dev.infomaniak.ch/`);
-
-        console.log('🚀 ~ generateCodeChallenge URL', `${IKConstants.LOGIN_URL}authorize?code_challenge=${codeChallenge}${infinite ? '' : '&access_type=offline'}&code_challenge_method=S256&client_id=${IKConstants.CLIENT_ID}&response_type=code&redirect_uri=${redirectTo}`);
-        window.location.assign(`https://login.preprod.dev.infomaniak.ch/authorize?code_challenge=${codeChallenge}${infinite ? '' : '&access_type=offline'}&code_challenge_method=S256&client_id=${IKConstants.CLIENT_ID}&response_type=code&redirect_uri=https://infomaniak.kchat.preprod.dev.infomaniak.ch/`);
+        window.location.assign(`${IKConstants.LOGIN_URL}authorize?code_challenge=${codeChallenge}${infinite ? '' : '&access_type=offline'}&code_challenge_method=S256&client_id=${IKConstants.CLIENT_ID}&response_type=code&redirect_uri=${redirectTo}`);
     }).catch(() => {
         console.log('[login/utils > getChallengeAndRedirectToLogin] error redirect');
     });
@@ -158,7 +154,7 @@ export function checkIKTokenExpiresSoon(): boolean {
 }
 
 export function isDefaultAuthServer() {
-    return true;
+    return window.location.origin === v2DefaultAuthServer;
 }
 
 function storeTokenV2(tokenData: {token: string; refreshToken?: string; expiresAt?: number}) {
