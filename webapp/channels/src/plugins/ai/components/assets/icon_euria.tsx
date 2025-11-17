9d76b3146168d72611242ff531f1d2f9d08ac6e1
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-11-17 11:18:42 +0100

    restore: filename in comp

--- webapp/channels/src/plugins/ai/components/assets/icon_euria.tsx
+++ webapp/channels/src/plugins/ai/components/assets/icon_euria.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import iconEuria from 'images/ik/euria-logo.gif';
+import iconEuria from 'images/euria_logo.gif';
 
 export interface IconEuriaProps {
     className?: string;
