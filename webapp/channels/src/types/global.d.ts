a91d8546667367f4db56e5ed548bea00a2ae0811
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-12-17 11:23:00 +0100

    chore: declare wc type globally

--- webapp/channels/src/types/global.d.ts
+++ webapp/channels/src/types/global.d.ts
@@ -12,3 +12,17 @@ interface Window {
         tokenRequest: () => Promise<void>;
     };
 }
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        'wc-contact-sheet': unknown;
+        'wc-ksuite-pro-upgrade-banner': any;
+        'wc-ksuite-pro-upgrade-dialog': any;
+        'wc-ksuite-pro-upgrade-button': any;
+        'wc-ksuite-pro-upgrade-tag': any;
+        'wc-mail-attachment': any;
+        'wc-ksuite-pro-upgrade-compact-tag': any;
+        'wc-icon': any;
+    }
+}
+
