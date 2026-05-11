6b6f4f997f42403743e497cf50617b0499638fcb
Aurélien Sberro <aurelien.sberro@infomaniak.com>
2026-05-11 12:16:31 +0200

    chore(cleanup): Remove unused backend code

--- server/public/pluginapi/experimental/common/logapi.go
+++
@@ -1,8 +0,0 @@
-package common
-
-type LogAPI interface {
-	LogError(message string, keyValuePairs ...interface{})
-	LogWarn(message string, keyValuePairs ...interface{})
-	LogInfo(message string, keyValuePairs ...interface{})
-	LogDebug(message string, keyValuePairs ...interface{})
-}
--- server/public/pluginapi/experimental/panel/settings/store.go
+++
@@ -1,7 +0,0 @@
-package settings
-
-// SettingStore defines the behavior needed to set and get settings
-type SettingStore interface {
-	SetSetting(userID, settingID string, value interface{}) error
-	GetSetting(userID, settingID string) (interface{}, error)
-}
