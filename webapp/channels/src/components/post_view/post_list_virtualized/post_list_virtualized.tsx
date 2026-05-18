def6cfb1f000ad58a969a0c53c3a1c24094ae052
Aurélien Sberro <aurelien.sberro@infomaniak.com>
2026-05-18 14:23:00 +0200

    fix(post-list): Backport auto-scroll fix from upstream [RM-549521]

--- webapp/channels/src/components/post_view/post_list_virtualized/post_list_virtualized.tsx
+++ webapp/channels/src/components/post_view/post_list_virtualized/post_list_virtualized.tsx
@@ -134,9 +134,6 @@ type State = {
     postListIds: string[];
     topPostId: string;
     postMenuOpened: boolean;
-    dynamicListStyle: {
-        willChange: string;
-    };
     initScrollCompleted: boolean;
     initScrollOffsetFromBottom: number;
     showSearchHint: boolean;
@@ -171,9 +168,6 @@ export default class PostList extends React.PureComponent<Props, State> {
             postListIds: [channelIntroMessage],
             topPostId: '',
             postMenuOpened: false,
-            dynamicListStyle: {
-                willChange: 'transform',
-            },
             initScrollCompleted: false,
             initScrollOffsetFromBottom: 0,
             showSearchHint: false,
@@ -265,7 +259,7 @@ export default class PostList extends React.PureComponent<Props, State> {
         EventEmitter.removeListener(EventTypes.POST_LIST_SCROLL_TO_BOTTOM, this.scrollToLatestMessages);
     }
 
-    static getDerivedStateFromProps(props: Props, state: State) {
+    static getDerivedStateFromProps(props: Props) {
         const postListIds = props.postListIds || [];
         let newPostListIds;
 
@@ -289,25 +283,6 @@ export default class PostList extends React.PureComponent<Props, State> {
             postListIds: newPostListIds,
         };
 
-        if (props.isMobileView !== state.isMobileView) {
-            nextState.isMobileView = props.isMobileView;
-
-            const dynamicListStyle = state.dynamicListStyle;
-            if (state.postMenuOpened) {
-                if (!props.isMobileView && dynamicListStyle.willChange === 'unset') {
-                    nextState.dynamicListStyle = {
-                        ...dynamicListStyle,
-                        willChange: 'transform',
-                    };
-                } else if (props.isMobileView && dynamicListStyle.willChange === 'transform') {
-                    nextState.dynamicListStyle = {
-                        ...dynamicListStyle,
-                        willChange: 'unset',
-                    };
-                }
-            }
-        }
-
         return nextState;
     }
 
@@ -316,17 +291,8 @@ export default class PostList extends React.PureComponent<Props, State> {
     };
 
     togglePostMenu = (opened: boolean) => {
-        let dynamicListStyle = this.state.dynamicListStyle;
-        if (this.props.isMobileView) {
-            dynamicListStyle = {
-                ...dynamicListStyle,
-                willChange: opened ? 'unset' : 'transform',
-            };
-        }
-
         this.setState({
             postMenuOpened: opened,
-            dynamicListStyle,
         });
     };
 
@@ -673,8 +639,6 @@ export default class PostList extends React.PureComponent<Props, State> {
     };
 
     render() {
-        const {dynamicListStyle} = this.state;
-
         return (
             <div
                 className='a11y__region'
@@ -728,7 +692,7 @@ export default class PostList extends React.PureComponent<Props, State> {
                                             initScrollToIndex={this.initScrollToIndex}
                                             canLoadMorePosts={this.props.actions.canLoadMorePosts}
                                             innerRef={this.postListRef}
-                                            style={{...virtListStyles, ...dynamicListStyle}}
+                                            style={virtListStyles}
                                             innerListStyle={postListStyle}
                                             initRangeToRender={this.initRangeToRender}
                                             loaderId={PostListRowListIds.OLDER_MESSAGES_LOADER}
